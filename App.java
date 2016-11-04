7620f45ebb4554afc9cffcc7618260791ee3f341
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-11-05 00:49:50 +0100

    ~*

--- App.java
+++ App.java
@@ -1,7 +1,20 @@
+import java.io.*;
+import java.net.*;
 import org.takes.http.Exit;
 import org.takes.http.FtBasic;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
 public final class App{
  public static void main(final String... a) throws Exception{
-     new FtBasic(new TkFork(new FkRegex("/","HelloWorld!")),2805).start(Exit.NEVER);}}
+  new FtBasic(new TkFork(new FkRegex("/","HelloWorld!")),2805).start(Exit.NEVER);
+  //     https://systembash.com/a-simple-java-udp-server-and-udp-client/
+  /*  public Response act(final RqRegex request) throws IOException {
+      final File file = new File(
+				 request.matcher().group("path")
+				 );
+      return new RsHTML(
+			FileUtils.readFileToString(file, Charsets.UTF_8)
+			);
+			}*/
+
+}}
