70d1bcfd0c8c363fb3c3d1a7bfa7e05eb7d0bbba
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-11-05 00:40:47 +0100

    ~*

--- App.java
+++ App.java
@@ -2,10 +2,6 @@ import org.takes.http.Exit;
 import org.takes.http.FtBasic;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
-public final class App {
-    public static void main(final String... args) throws Exception {
-	new FtBasic(
-		    new TkFork(new FkRegex("/", "hello, world!")), 8080
-		    ).start(Exit.NEVER);
-    }
-}
+public final class App{
+ public static void main(final String... a) throws Exception{
+  new FtBasic(new TkFork(new FkRegex("/" "HelloWorld!")),2805).start(Exit.NEVER);}}
