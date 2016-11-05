34f928873225a525dd26cc801496c2aaf99d32d2
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-11-05 01:14:45 +0100

    ~*

--- App.java
+++ App.java
@@ -11,9 +11,7 @@ import org.takes.facets.fork.TkFork;
 public final class App{
  public static void main(final String... a) throws Exception{
   new FtBasic(new TkFork(new FkRegex("/",
-   new TkRegex(){
-    @Override public Response act(final RqRegex rq) throws IOException{
-     new RsHtml("<html>Hello, world!</html>");}}
-				     )),2805).start(Exit.NEVER);}
+   new TkRegex(){@Override public Response act(final RqRegex rq) throws IOException{
+    return new RsHtml("<html>Hello, world!</html>");}})),2805).start(Exit.NEVER);}
   //     https://systembash.com/a-simple-java-udp-server-and-udp-client/
 }
