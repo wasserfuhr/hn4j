91166af363cfbcd92f04edaa7970b25069ae53d4
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-11-05 01:29:26 +0100

    ~*

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import org.takes.Request;
 import org.takes.Response;
 import org.takes.http.Exit;
 import org.takes.http.FtBasic;
@@ -12,6 +13,7 @@ import org.takes.facets.fork.TkRegex;
 public final class App{
  public static void main(final String... a) throws Exception{
   new FtBasic(new TkFork(new FkRegex("/",
-   new TkRegex(){@Override public Response act(final RqRegex rq) throws IOException{
+   new TkRegex(){@Override public Response act(final Request rq) throws IOException{
+       //   new TkRegex(){@Override public Response act(final RqRegex rq) throws IOException{
     return new RsHtml("Hello"+new RqForm(rq).param("msg"));}})),2805).start(Exit.NEVER);}}
   //https://systembash.com/a-simple-java-udp-server-and-udp-client/
