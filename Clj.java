82717ea0cedffa3ab614ba666a80740a0f46dc6a
RaWa <wasserfuhr@yahoo.com>
2016-11-05 16:36:02 +0100

    ~*

--- Clj.java
+++ Clj.java
@@ -1,17 +1,15 @@
-﻿<%@page contentType="text/html;charset=UTF-8"%>
-<%@page import="
-java.io.PushbackReader,
-java.io.StringReader,
-clojure.lang.Compiler,
-clojure.lang.IFn,
-clojure.lang.LispReader,
-clojure.lang.RT"%><%
-try {
- RT.loadResourceScript("hiccup/core.clj");
- RT.loadResourceScript("clojure/data/json.clj");
- String c="(fn [rq rs] ((eval (read-string (slurp \"https://dresdenlabs.appspot.com/boot\"))) rq rs))";
- PushbackReader pr = new PushbackReader(new StringReader(c));
- Object rootHandlerExpr=LispReader.read( pr, true, null, false);
- IFn rootHandlerFn=(IFn) Compiler.eval( rootHandlerExpr);
-%><%=rootHandlerFn.invoke(request,response)%><%
-} finally { }%>
+﻿import java.io.PushbackReader;
+import java.io.StringReader;
+import clojure.lang.Compiler;
+import clojure.lang.IFn;
+import clojure.lang.LispReader;
+import clojure.lang.RT;
+public class Clj{
+ public static void exec(byte b[])throws Exception{
+  RT.loadResourceScript("hiccup/core.clj");
+  RT.loadResourceScript("clojure/data/json.clj");
+  String c="(fn [rq rs] ((eval (read-string (slurp \"https://dresdenlabs.appspot.com/boot\"))) rq rs))";
+  PushbackReader pr = new PushbackReader(new StringReader(c));
+  Object rootHandlerExpr=LispReader.read( pr, true, null, false);
+  IFn rootHandlerFn=(IFn) Compiler.eval( rootHandlerExpr);
+  rootHandlerFn.invoke(request,response);}}
