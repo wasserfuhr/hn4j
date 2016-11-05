0142159e733c2b50030632dec0e2ed7fe631bac2
RaWa <wasserfuhr@yahoo.com>
2016-11-05 16:44:59 +0100

    ~*

--- Clj.java
+++ Clj.java
@@ -1,13 +1,13 @@
-﻿import java.io.PushbackReader;
+import java.io.PushbackReader;
 import java.io.StringReader;
 import clojure.lang.Compiler;
 import clojure.lang.IFn;
 import clojure.lang.LispReader;
 import clojure.lang.RT;
 public class Clj{
- public static void exec(byte b[])throws Exception{
-  String c=new String(b);
-  PushbackReader pr = new PushbackReader(new StringReader(c));
-  Object expr=LispReader.read(pr,true,null,false);
-  IFn fn=(IFn) Compiler.eval(expr);
+public static void exec(byte b[])throws Exception{
+String c=new String(b);
+PushbackReader pr = new PushbackReader(new StringReader(c));
+Object expr=LispReader.read(pr,true,null,false);
+IFn fn=(IFn) Compiler.eval(expr);
   fn.invoke();}}
