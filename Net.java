57a929ed88c24c0d7f1f23dbea922557e8053ab7
RaWa <wasserfuhr@yahoo.com>
2016-11-05 12:58:21 +0100

    ~*

--- Net.java
+++ Net.java
@@ -19,10 +19,8 @@ public class Net{
    System.out.print(String.format("7%x",System.currentTimeMillis()/1000-0x5608aa2b));
    System.out.print(String.format("%x",ct));
    String i[]=rp.getAddress().getHostAddress().substring(1).split("\\.");
-   System.out.print(String.format("%2x",new Integer(i[0])));
-   System.out.print(String.format("%2x",new Integer(i[1])));
-   System.out.print(String.format("%2x",new Integer(i[2])));
-   System.out.print(String.format("%2x",new Integer(i[3])));
+   for(int i=0;i<4;i+++)
+    System.out.print(String.format("%2x",new Integer(i[i])));
    for(byte b:n)System.out.print(String.format("%02x",b&0xff));
    MessageDigest digest = MessageDigest.getInstance("SHA-256");
    byte[] hash = digest.digest(n);
