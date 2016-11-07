18d137a250d227f1840b610233d26ac02b095b99
RaWa <wasserfuhr@yahoo.com>
2016-11-07 02:54:59 +0100

    Update Clj.java

--- Clj.java
+++ Clj.java
@@ -4,11 +4,6 @@ import java.net.DatagramPacket;
 import clojure.lang.Compiler;
 import clojure.lang.IFn;
 import clojure.lang.LispReader;
-import clojure.lang.RT;
-public class Clj{
-public static void exec(DatagramPacket p)throws Exception{
-String c=new String(p.getData());
-PushbackReader pr = new PushbackReader(new StringReader(c));
-Object expr=LispReader.read(pr,true,null,false);
-IFn fn=(IFn) Compiler.eval(expr);
-  fn.invoke(p);}}
+public class Clj{public static void exec(DatagramPacket p)throws Exception{
+Object e=LispReader.read(new PushbackReader(new StringReader(new String(p.getData()))),true,null,false);
+((IFn)Compiler.eval(e)).invoke(p);}}
