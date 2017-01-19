565fd5205f36bffc75b76c03e524b11e849b72c5
RaWa <wasserfuhr@yahoo.com>
2017-01-20 00:08:11 +0100

    ~snd.sh

--- snd.sh
+++ snd.sh
@@ -1,5 +1,5 @@
 curl -s --user 'api:key-fc1b979178be00c42abe0e3f7f5eb120'\
- https://api.mailgun.net/v3/samples.mailgun.org/messages\
+ https://api.mailgun.net/v3/sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org\
  -F from='RaWa<wasserfuhr@yahoo.com>'\
  -F to='wasserfuhr@yahoo.com'\
  -F subject='Hello'\
