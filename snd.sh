a6038a4a9ec1875cb6925ee002d2f960f014f066
RaWa <wasserfuhr@yahoo.com>
2017-01-20 00:06:34 +0100

    +snd.sh

--- snd.sh
+++ snd.sh
@@ -1,4 +1,4 @@
-curl -s --user 'api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0'\
+curl -s --user 'api:key-fc1b979178be00c42abe0e3f7f5eb120'\
  https://api.mailgun.net/v3/samples.mailgun.org/messages\
  -F from='RaWa<wasserfuhr@yahoo.com>'\
  -F to='wasserfuhr@yahoo.com'\
