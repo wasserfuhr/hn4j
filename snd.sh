de6562c7fdc272cabed656131ce391a1933c91c1
RaWa <wasserfuhr@yahoo.com>
2017-01-20 00:02:13 +0100

    +snd.sh

---
+++ snd.sh
@@ -0,0 +1,6 @@
+curl -s --user 'api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0' \
+     https://api.mailgun.net/v3/samples.mailgun.org/messages \
+     -F from='Excited User <excited@samples.mailgun.org>' \
+     -F to='devs@yahoo.com' \
+     -F subject='Hello' \
+      -F text='Testing some Mailgun awesomeness!'
