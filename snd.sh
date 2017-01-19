b388087a5f8104ae0e1075c8f3aa9775afa5487b
RaWa <wasserfuhr@yahoo.com>
2017-01-20 00:03:25 +0100

    +snd.sh

--- snd.sh
+++ snd.sh
@@ -1,6 +1,6 @@
-curl -s --user 'api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0' \
-     https://api.mailgun.net/v3/samples.mailgun.org/messages \
-     -F from='Excited User <excited@samples.mailgun.org>' \
-     -F to='devs@mailgun.net' \
-     -F subject='Hello' \
-     -F text='Testing some Mailgun awesomeness!'
+curl -s --user 'api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0'\
+ https://api.mailgun.net/v3/samples.mailgun.org/messages\
+ -F from='Excited User <excited@samples.mailgun.org>'\
+ -F to='wasserfuhr@yahoo.com'\
+ -F subject='Hello'\
+ -F text='Testing some Mailgun awesomeness!'
