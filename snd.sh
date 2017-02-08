7d2de4b39c860ea0ba8e1ab4dc330eca80fda833
RaWa <wasserfuhr@yahoo.com>
2017-02-08 01:51:30 +0100

    ~snd.sh

--- snd.sh
+++ snd.sh
@@ -2,7 +2,6 @@ curl -s --user 'api:key-fc1b979178be00c42abe0e3f7f5eb120'\
  https://api.mailgun.net/v3/sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org/messages\
  -F from='RaWa<mailgun@sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org>'\
  -F to='wasserfuhr@yahoo.com'\
- -F subject='SemperGeil?'\
- -F text='Testing some Mailgun awesomeness!'
-
-#-F from='RaWa<wasserfuhr@yahoo.com>'\
+ -F subject='AlphaTest'\
+ -F text='please reply at https://floatingboat2013.appspot.com/live'
+#-F from='RaWa<wasserfuhr@yahoo.com>'
