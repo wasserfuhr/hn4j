a1220a659b5a82f9c29a7796ee00e5c89b5332b5
RaWa <wasserfuhr@yahoo.com>
2017-02-08 01:39:50 +0100

    ~snd.sh

--- snd.sh
+++ snd.sh
@@ -1,6 +1,8 @@
 curl -s --user 'api:key-fc1b979178be00c42abe0e3f7f5eb120'\
  https://api.mailgun.net/v3/sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org/messages\
- -F from='RaWa<wasserfuhr@yahoo.com>'\
+ -F from='RaWa<mailgun@sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org>'\
  -F to='wasserfuhr@yahoo.com'\
  -F subject='SemperGeil?'\
  -F text='Testing some Mailgun awesomeness!'
+
+#-F from='RaWa<wasserfuhr@yahoo.com>'\
