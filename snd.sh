10bd52a89b6c189065b017b46583ab170dcf5c80
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2017-02-08 03:22:31 +0100

    ~snd.sh

--- snd.sh
+++ snd.sh
@@ -1,7 +1,7 @@
-curl -s --user 'api:key-fc1b979178be00c42abe0e3f7f5eb120'\
+curl -k -s --user 'api:key-fc1b979178be00c42abe0e3f7f5eb120'\
  https://api.mailgun.net/v3/sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org/messages\
  -F from='RaWa<mailgun@sandbox5b800841f49140a3a394c7e5a055a020.mailgun.org>'\
- -F to='wasserfuhr@yahoo.com'\
+ -F to='$1'\
  -F subject='AlphaTest'\
  -F text='please reply at https://floatingboat2013.appspot.com/live'
 #-F from='RaWa<wasserfuhr@yahoo.com>'
