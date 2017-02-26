9f7be9e9c388758a2fd2112f58db5b3c523a81f9
RaWa <wasserfuhr@yahoo.com>
2017-02-26 21:57:45 +0100

    +WiDaInstance.clj

---
+++ WiDaInstance.clj
@@ -0,0 +1,15 @@
+(fn[rq rs](let[
+q(.getParameter rq"q")
+Q(if q q"Q107006")
+s(slurp(str"https://www.wikidata.org/wiki/Special:EntityData/"Q".json"))]
+(if(.startsWith s"<!DOCTYPE")""
+(:id
+(:value(:datavalue
+(:mainsnak
+(first
+(:P31
+(:claims
+ ((keyword Q)
+   (:entities
+      (clojure.contrib.json/read-json s)
+        ))))))))))))
