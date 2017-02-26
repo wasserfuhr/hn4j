1b470089b27a810b3e838e78aa7030958a7553b5
RaWa <wasserfuhr@yahoo.com>
2017-02-27 00:15:32 +0100

    +WiDaMan.clj

---
+++ WiDaMan.clj
@@ -0,0 +1,17 @@
+(ns example
+ (:require [clojure.data.json :as json]))
+(apply str
+(filter #(.endsWith %"Q5")
+(map(fn[r](let[
+ s(try(slurp(str"https://www.wikidata.org/wiki/Special:EntityData/Q"r".json"))
+ (catch Exception e nil))]
+(if s(str" "r" "(:id
+(:value(:datavalue
+(:mainsnak
+(first
+(:P31
+(:claims
+ ((keyword(str "Q"r))
+   (:entities
+    (json/read-str s :key-fn keyword)))))))))))"")))
+ (range 1 0x100))))
