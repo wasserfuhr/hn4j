0345fb06e7e38533cee5c1b5178f4e3f18ae2199
RaWa <wasserfuhr@yahoo.com>
2017-02-26 22:54:42 +0100

    ~WiDaInstance.clj

--- WiDaInstance.clj
+++ WiDaInstance.clj
@@ -1,12 +1,14 @@
 (ns example
-  (:require [clojure.contrib.json]))
+  (:require [clojure.contrib.json :as json]))
 
      
 ;    (:require [clojure.data.json :as json]))
 
 ;(apply str
 
-(clojure.contrib.json/read-json
+;(clojure.contrib.json/read-json
+
+(json/read-json
  (slurp(str"https://www.wikidata.org/wiki/Special:EntityData/Q42.json")))
 
 (map(fn[r]
