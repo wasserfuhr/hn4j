83329ffd8407bfb339f52a01274bf9d083219a73
RaWa <wasserfuhr@yahoo.com>
2017-02-27 00:26:03 +0100

    ~WiDaMan.clj

--- WiDaMan.clj
+++ WiDaMan.clj
@@ -4,8 +4,6 @@
 (apply str
 (map(fn[r](let[
  s(slurp(str"https://www.wikidata.org/wiki/Special:EntityData/"r".json"))]
-(:value(:en(:labels
- ((keyword r)
-   (:entities
-    (json/read-str s :key-fn keyword))))))))
+(str r" "(:value(:en(:labels((keyword r)(:entities
+    (json/read-str s :key-fn keyword)))))))))
    (.split "Q76 Q80 Q91 Q157 Q181 Q185 Q186 Q192 Q206 Q207 Q254 Q255" " ")))
