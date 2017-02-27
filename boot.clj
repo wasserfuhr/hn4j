fe7e9d569c88fc914a191415f8b5b9456494da84
RaWa <wasserfuhr@yahoo.com>
2017-02-27 07:22:36 +0100

    +boot.clj

--- boot.clj
+++ boot.clj
@@ -1,1 +1,6 @@
-(fn[rq rs](hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α"]][:body"α"]]))
+(fn[rq rs](let[
+ t(-(.getTime(java.util.Date.))1443408427000)
+  d(long(/ t 1000.0))
+   r(- t(* d 1000))];mod?
+    (format"%02x.%02x"d(quot(* 256 r)1000))))
+;    (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α"]][:body"α"]]))
