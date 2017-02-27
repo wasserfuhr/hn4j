d646517924e251c7a7f9cfa4f0ac9c89b72c11e9
RaWa <wasserfuhr@yahoo.com>
2017-02-27 07:33:29 +0100

    +boot.clj

--- boot.clj
+++ boot.clj
@@ -1,6 +1,7 @@
 (fn[rq rs](let[
  t(-(.getTime(java.util.Date.))1443408427000)
-  d(long(/ t 1000.0))
-   r(- t(* d 1000))];mod?
-    (format"%02x.%02x"d(quot(* 256 r)1000))))
-;    (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α"]][:body"α"]]))
+ d(long(/ t 1000.0))
+ r(- t(* d 1000));mod?
+ a(format"%02x.%02x"d(quot(* 256 r)1000))]
+(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α"]]
+[:body"αt"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"α"]" approaching β."]]))
