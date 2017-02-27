b7a384903f9ce109f96551d670031acbe96299b4
RaWa <wasserfuhr@yahoo.com>
2017-02-27 07:56:46 +0100

    +boot.clj

--- boot.clj
+++ boot.clj
@@ -2,6 +2,7 @@
  t(-(.getTime(java.util.Date.))1443408427000)
  d(long(/ t 1000.0))
  r(- t(* d 1000));mod?
- a(format"%02x.%02x"d(quot(* 256 r)1000))](println"HelloLog")
+ a(format"%02x.%02x"d(quot(* 256 r)1000))]
+(println a " "(.getRemoteHost rq)" "(.getRequestURI rq))
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
 [:body"αt"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"α"]" approaching β."]])))
