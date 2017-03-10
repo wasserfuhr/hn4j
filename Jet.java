2e1ff530d0a15dab8b44117d75ed63d61d7d7cb0
RaWa <wasserfuhr@yahoo.com>
2017-03-10 16:01:11 +0100

    ~Jet.java

--- Jet.java
+++ Jet.java
@@ -3,6 +3,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import clojure.lang.*;
 // java -cp jetty-all-8.1.9.v20130131.jar:clojure-1.8.0.jar:hiccup-2.0.0-alpha1.jar:javax.servlet-api-3.1.0.jar:. Jet &
 //http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
@@ -24,6 +25,25 @@ public class Jet extends AbstractHandler{
  public static void main(String[] a)throws Exception{
   //new Ur().start();
   Server s = new Server(80);
+
+  //https://dzone.com/articles/adding-ssl-support-embedded
+  ServerConnector connector = new ServerConnector(server);
+  connector.setPort(9999);
+
+  HttpConfiguration https = new HttpConfiguration();
+  https.addCustomizer(new SecureRequestCustomizer());
+  SslContextFactory sslContextFactory = new SslContextFactory();
+  sslContextFactory.setKeyStorePath(EmbeddedServer.class.getResource(
+								     "/keystore.jks").toExternalForm());
+  sslContextFactory.setKeyStorePassword("123456");
+  sslContextFactory.setKeyManagerPassword("123456");
+  ServerConnector sslConnector = new ServerConnector(server,
+						     new SslConnectionFactory(sslContextFactory, "http/1.1"),
+						     new HttpConnectionFactory(https));
+  sslConnector.setPort(9998);
+
+  server.setConnectors(new Connector[] { connector, sslConnector });
+  
   s.setHandler(new Jet());
   s.start();
   s.join();}}
