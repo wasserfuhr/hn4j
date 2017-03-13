12e02dcdf8462040a146df8a8c467d72a1c78037
RaWa <wasserfuhr@yahoo.com>
2017-03-13 15:01:52 +0100

    ~boot.clj

--- boot.clj
+++ boot.clj
@@ -7,8 +7,8 @@
  a(format"%02x.%02x"d(quot(* 256 r)1000))]
 ;(.println *err*"\007")
 (println a i(.getRequestURI rq))
-(if(.startsWith(.getRequestURI rq)"/a9e3")
- (.sendRedirect
+(if(.startsWith(.getRequestURI rq)"/a9e")
+ (.sendRedirect rs
   "https://upload.wikimedia.org/wikipedia/commons/6/6b/Gordon_Moore.jpg")
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
 [:body"αt"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"α"]" approaching β."]]))))
