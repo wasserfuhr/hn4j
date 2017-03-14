bfe4d4d7ef5d2be7cbf1078a9589002acb57e498
RaWa <wasserfuhr@yahoo.com>
2017-03-14 14:34:58 +0100

    ~WiDaSize.clj

--- WiDaSize.clj
+++ WiDaSize.clj
@@ -1,7 +1,7 @@
 (apply str(map(fn[l](str
 ;(get(.split l" ")4)
 " "
-(subs l 40)
+(first(.split(subs l 42)"\\."))
 ;(first(.split(get(.split l" ")8)"\\."))
 ))
 ;(line-seq (java.io.BufferedReader. *in*)))))
