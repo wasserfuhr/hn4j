cd0c1187f1288244310f77a149e68d14e242ac9e
RaWa <wasserfuhr@yahoo.com>
2017-03-14 13:04:12 +0100

    ~WiDaSize.clj

--- WiDaSize.clj
+++ WiDaSize.clj
@@ -1,5 +1,6 @@
 (apply str(map(fn[l](str
-;(get(.split l" ")4)
+(get(.split l" ")4)
 " "
-(first(.split(get(.split l" ")8)"\\."))))
+;(first(.split(get(.split l" ")8)"\\."))
+))
  (.split(slurp"qls")"\n")))
