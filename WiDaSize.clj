a5090ae77ed257a4870422f60c82af5a795f71b2
RaWa <wasserfuhr@yahoo.com>
2017-03-14 13:04:36 +0100

    ~WiDaSize.clj

--- WiDaSize.clj
+++ WiDaSize.clj
@@ -1,6 +1,6 @@
-(apply str(map(fn[l](str
+(print(apply str(map(fn[l](str
 (get(.split l" ")4)
 " "
 ;(first(.split(get(.split l" ")8)"\\."))
 ))
- (.split(slurp"qls")"\n")))
+ (.split(slurp"qls")"\n"))))
