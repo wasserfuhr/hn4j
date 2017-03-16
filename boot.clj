180c9e81691bf74450cbd4c7021a3bab18c1c6e7
RaWa <wasserfuhr@yahoo.com>
2017-03-16 18:04:25 +0100

    ~boot.clj

--- boot.clj
+++ boot.clj
@@ -15,5 +15,5 @@
   (.sendRedirect rs
    "https://upload.wikimedia.org/wikipedia/commons/6/6b/Gordon_Moore.jpg")
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
-[:body"αt"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
+[:body"&alpha;t"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
 [:br]"try "[:a{:href"/memo"}"AlphaMemory"]]]))))))
