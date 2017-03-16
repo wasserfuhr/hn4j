c391e779c033ddf0bc32ba06766b41373ac7354a
RaWa <wasserfuhr@yahoo.com>
2017-03-16 14:23:25 +0100

    ~WiDaPic.clj

--- WiDaPic.clj
+++ WiDaPic.clj
@@ -1,7 +1,5 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[Q"Q42]
+(let[Q"Q42"
+s(slurp(str"q/"Q".json"))]
 (:value(:datavalue(:mainsnak(first(:P18 (:claims
- ((keyword Q)(:entities
-      (clojure.contrib.json/read-json
-          (slurp(str"q/"Q".json")))
-	    ))))))))" ""_")"UTF-8"))))
+ ((keyword Q)(:entities(json/read-str s :key-fn keyword))))))))))
