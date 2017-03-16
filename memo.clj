0e2e3ec1673963d09a9f3f1a67a643d414bd7490
RaWa <wasserfuhr@yahoo.com>
2017-03-17 00:18:41 +0100

    +memo.clj

--- memo.clj
+++ memo.clj
@@ -63,9 +63,9 @@ function abc(l){for(i=0;i<p.length;i++)
 
 [:div
 
-[:div#Q213{:style"float:left;background-size:105px 128px;background-repeat:no-repeat;background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/197px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg)"}[:p{:style"background-color:#fff"}"George"[:br]"Washington"]]
+[:div#Q213{:style"height:128px;float:left;background-size:105px 128px;background-repeat:no-repeat;background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/197px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg)"}[:p{:style"background-color:#fff"}"George"[:br]"Washington"]]
 
-[:div#Q2311{:style"float:left;background-size:105px 128px;background-repeat:no-repeat;background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/197px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg)"}"George"[:br]"Washingtonnn"]
+[:div#Q2311{:style"height:128px;float:left;background-size:105px 128px;background-repeat:no-repeat;background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/197px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg)"}"George"[:br]"Washingtonnn"]
 
 ;[:div#Q76{:style"background-size:105px 128px;background-repeat:no-repeat;background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/192px-President_Barack_Obama.jpg)"}[:p{:style"background-color:#fff"}"George"[:br]"Washington"]]
 
