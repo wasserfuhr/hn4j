1a6fd92fec01a615a79a216088e8f06d2037703e
RaWa <wasserfuhr@yahoo.com>
2017-03-16 14:58:17 +0100

    ~GetWpPic.clj

--- GetWpPic.clj
+++ GetWpPic.clj
@@ -1,7 +1,7 @@
-(fn[rq rs](let[
-f"Gilbert Stuart Williamstown Portrait of George Washington.jpg"
-;"Henry Hubchen.jpg"
-;Sergey Brin Ted 2010.jpg
+(map(fn[l](let[
+s(.indexOf l" ")
+s0(subs l 0 s)
+s1(subs l s)
 e(java.net.URLEncoder/encode (.replaceAll f" ""_"))
 s(slurp(str"https://en.wikipedia.org/wiki/File:"e))
 i0(.indexOf s "mw-filepage-other-resolutions")
@@ -11,4 +11,7 @@ r(.split t"class=\"mw-thumbnail-link\">")
 u(.split(second r)" ")
 l(.indexOf(second r)"/thumb/")
 ](str(subs(second r)(+ l 9)(+ l 11))" "(first u)" "(get u 2))))
-;]t))
+(.split"Q23 Gilbert Stuart Williamstown Portrait of George Washington.jpg
+QX Henry Hubchen.jpg"
+;Sergey Brin Ted 2010.jpg
+"\n"))
