77a75675a4401aa50791f9b805a9dcfd4172103b
RaWa <wasserfuhr@yahoo.com>
2017-03-16 23:42:25 +0100

    +memo.clj

--- memo.clj
+++ memo.clj
@@ -63,7 +63,7 @@ function abc(l){for(i=0;i<p.length;i++)
 
 [:div
 
-[:div{:style"float:left"}[:span#tQ23{:style"display:none;z-index:1"}][:br]
+[:div{:style"float:left;position:relative"}[:span#tQ23{:style"display:none;z-index:1;position:absolute;top:0;left:0"}]
 [:img#Q23{:width 105 :height 128 :style"border:#fff solid 3px;z-index:0;position:absolute;top:0;left:0" :onclick"d('Q23')"
 :src"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/197px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"}]]
 
