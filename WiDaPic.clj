5be32e2de8944bc3c9b8d4a578819ecfa598d7b8
RaWa <wasserfuhr@yahoo.com>
2017-03-16 14:27:01 +0100

    ~WiDaPic.clj

--- WiDaPic.clj
+++ WiDaPic.clj
@@ -3,5 +3,5 @@
 s(slurp(str"q/"q".json"))]
 (println
 (:value(:datavalue(:mainsnak(first(:P18 (:claims
- ((keyword Q)(:entities(json/read-str s :key-fn keyword))))))))))))
+ ((keyword q)(:entities(json/read-str s :key-fn keyword))))))))))))
  (.split"Q7747 Q76 Q937 Q7200 Q6294 Q352 Q8016 Q12823 Q5879 Q8704 Q8023 Q7243 Q991 Q5152 Q255 Q692 Q254 Q1339 Q9682 Q855 Q5383 Q762 Q11571 Q5592 Q567 Q23 Q882"" "))
