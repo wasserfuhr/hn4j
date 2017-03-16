03a8204e3cf8bb0991c9f21061e4e810d9a1f024
RaWa <wasserfuhr@yahoo.com>
2017-03-16 15:01:46 +0100

    ~GetWpPic.clj

--- GetWpPic.clj
+++ GetWpPic.clj
@@ -12,6 +12,13 @@ u(.split(second r)" ")
 l(.indexOf(second r)"/thumb/")
 ](str s0" "(subs(second r)(+ l 9)(+ l 11))" "(first u)" "(get u 2))))
 (.split"Q23 Gilbert Stuart Williamstown Portrait of George Washington.jpg
-QX Henry Hubchen.jpg"
-;Sergey Brin Ted 2010.jpg
+QQ76 President Barack Obama.jpg
+Q937 Albert Einstein Head.jpg
+Q7200 Portrait of Alexander Pushkin (Orest Kiprensky, 1827).PNG
+Q6294 HillaryPA.jpg
+Q352 Bundesarchiv Bild 183-S33882, Adolf Hitler retouched.jpg
+Q8016 Sir Winston S Churchill.jpg
+Q12823 Susan Polgar 6.jpg
+QX Henry Hubchen.jpg
+QY Sergey Brin Ted 2010.jpg"
 "\n"))
