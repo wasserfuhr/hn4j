80cd758e7b563c09f4ca93402844e2bd3c57a552
RaWa <wasserfuhr@yahoo.com>
2017-03-16 16:16:43 +0100

    ~GetWpPic.clj

--- GetWpPic.clj
+++ GetWpPic.clj
@@ -21,10 +21,10 @@ w(Double.(get u 2))]
 ;https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/245px-Steve_Jobs_Headshot_2010-CROP.jpg
 ;(str s0" "k" "h" "w"\n"))
 (str"<img
-width="(Math/round(Math/min(*(/ h w)64)64.0))"
-height="(Math/round(Math/min(*(/ w h)64)64.0))"
+width="(Math/round(Math/min(*(/ h w)128)128.0))"
+height="(Math/round(Math/min(*(/ w h)128)128.0))"
 src='https://upload.wikimedia.org/wikipedia/commons/thumb/"(subs k 0 1)"/"k"/"e"/"(first u)"px-"e"'/>\n"))
-(catch Exception ex (str "*E*"s0" "e"\n")))))
+(catch Exception ex (str "*E*"s0"e\n")))))
 (.split
 "Q23 Gilbert Stuart Williamstown Portrait of George Washington.jpg
 Q42 douglas adams portrait cropped.jpg
