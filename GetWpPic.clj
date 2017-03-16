f9f815854ce6eb1f1ec6f1f6d0ed333c0d2b76e2
RaWa <wasserfuhr@yahoo.com>
2017-03-16 15:08:34 +0100

    ~GetWpPic.clj

--- GetWpPic.clj
+++ GetWpPic.clj
@@ -1,9 +1,9 @@
 (println(map(fn[l](let[
 s(.indexOf l" ")
 s0(subs l 0 s)
-s1(subs l s)]
+s1(subs l s)
+e(java.net.URLEncoder/encode (.replaceAll s1" ""_"))]
 (try(let[
-e(java.net.URLEncoder/encode (.replaceAll s1" ""_"))
 s(slurp(str"https://en.wikipedia.org/wiki/File:"e))
 i0(.indexOf s "mw-filepage-other-resolutions")
 i1(.indexOf(subs s i0)"</span>")
@@ -12,7 +12,7 @@ r(.split t"class=\"mw-thumbnail-link\">")
 u(.split(second r)" ")
 l(.indexOf(second r)"/thumb/")
 ](str s0" "(subs(second r)(+ l 9)(+ l 11))" "(first u)" "(get u 2)"\n"))
-(catch Exception e s0))))
+(catch Exception ex (str s0" "e)))))
 (.split"Q23 Gilbert Stuart Williamstown Portrait of George Washington.jpg
 QQ76 President Barack Obama.jpg
 Q937 Albert Einstein Head.jpg
