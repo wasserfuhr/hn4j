8c72fa2603fb90952b2927c204442670539edcbd
RaWa <wasserfuhr@yahoo.com>
2017-03-17 12:47:34 +0100

    ~boot.clj

--- boot.clj
+++ boot.clj
@@ -11,6 +11,8 @@
  ((eval(read-string(slurp"cookie.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/memo")
  ((eval(read-string(slurp"memo.clj")))rq rs)
+ (if(.startsWith(.getRequestURI rq)"/char")
+ ((eval(read-string(slurp"char.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/memoDev")
  ((eval(read-string(slurp"memoDev.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/a9e")
@@ -18,4 +20,4 @@
    "https://upload.wikimedia.org/wikipedia/commons/6/6b/Gordon_Moore.jpg")
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
 [:body"&alpha;t"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
-[:br]"try "[:a{:href"/memo"}"AlphaMemory"]]])))))))
+[:br]"try "[:a{:href"/memo"}"AlphaMemory"]]]))))))))
