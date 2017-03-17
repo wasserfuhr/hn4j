496975599f8d75129fa608d242dfb433fad6dcfa
RaWa <wasserfuhr@yahoo.com>
2017-03-17 14:58:37 +0100

    ~memoDev.clj

--- memoDev.clj
+++ memoDev.clj
@@ -26,7 +26,7 @@ function m(l,n){
  return false}
 
 setInterval(function(){
- document.getElementById('rot').style.paddingLeft=(40-(new Date().getTime()/2000)%40)+'px'
+ document.getElementById('rot').style.paddingLeft=(40-(new Date().getTime()/500)%40)+'px'
 
 // document.getElementById('Q991').style.backgroundImage='url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg/192px-Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg)'
 
