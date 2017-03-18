04c072394b66ba98c8f189981e668f73e3f5c651
RaWa <wasserfuhr@yahoo.com>
2017-03-18 14:02:42 +0100

    ~WiDaJs.clj

--- WiDaJs.clj
+++ WiDaJs.clj
@@ -1,2 +1,2 @@
-(apply str(map(fn[l](let[i(.indexOf l" ")](str"'"(subs l 0(- i 1))"':'"(subs l i)"'\n")))
- (.split(slurp"https://floatingboat2013.appspot.com/rawHead/WiDaMan")"\n")))
+(print(apply str(map(fn[l](let[i(.indexOf l" ")](str"'"(subs l 0(- i 1))"':'"(subs l i)"'\n")))
+ (.split(slurp"https://floatingboat2013.appspot.com/rawHead/WiDaMan")"\n"))))
