557bc81fd78c6f75c4f03e67c89f9655231ab015
RaWa <wasserfuhr@yahoo.com>
2017-03-18 22:16:38 +0100

    ~age.clj

--- age.clj
+++ age.clj
@@ -1,5 +1,5 @@
 (fn[rq rs](let[c(.getAttribute rq"c")v(.getAttribute rq"vars")t"AlphaHistory"
-s(.split(slurp"picSize.txt")"\")
+s(.split(slurp"picSize.txt")"\n")
 ]
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title t" &laquo; &alpha;"];(:can c)(:css c)(:vp c)
 [:link{:rel"stylesheet":type"text/css":href"https://dresdenlabs.appspot.com/css"}]
