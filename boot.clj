99315d4242e5f4944a327f09b4edc82c8b75b880
RaWa <wasserfuhr@yahoo.com>
2017-03-18 16:45:45 +0100

    ~boot.clj

--- boot.clj
+++ boot.clj
@@ -2,6 +2,12 @@
  ip(.split(.getRemoteHost rq)"\\.")
  i(apply str(map(fn[i](format"%02x"(Long. i)))ip))
  t(-(.getTime(java.util.Date.))1443408427000)
+ h(fn[m] ; https://gist.github.com/kisom/1698245
+   (let[h(java.security.MessageDigest/getInstance"SHA-256")]
+      (. h update m)(.digest h)))
+ f(fn[h]
+   (apply str
+       (map #(format "%02x"(bit-and % 0xff))h)))
  d(long(/ t 1000.0))
  r(- t(* d 1000));mod?
  a(format"%02x.%02x"d(quot(* 256 r)1000))]
