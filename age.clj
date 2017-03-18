c9c5653861a17da10a67ba603cbc541160d7fced
RaWa <wasserfuhr@yahoo.com>
2017-03-18 22:55:52 +0100

    ~age.clj

--- age.clj
+++ age.clj
@@ -13,14 +13,19 @@ n(.split(slurp"picName.txt")"\n")
  d(read-string(str"0x"q))
  p(filter(fn[i](.startsWith i(str d" ")))s)
  s(filter(fn[i](.startsWith i(str"Q"d" ")))n)]
- (if(and p s)(let[a(.split(first p)" ")
- b(.split(first s)" ")]
+ (if(and (not-empty p) (not-empty s))
+ (let[a(.split(first p)" ")
+  b(.split(first s)" ")
+
+  ]
 [:img{:id(str"i"q)
-:alt a; (second a)
-:title b;(second b)
+:width(read-string(str"0x"(get a 2)))
+:height(read-string(str"0x"(get a 3)))
+:alt(second a)
+:title(second b)
 }]))))
  (.split"5f3 5f6 5f8 5fc 391"" "))
- 
+
  [:img#rQ326{:height 60 :width 42
    :src"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0 /Eduardo_Frei_Chiledebate.jpg/166px-Eduardo_Frei_Chiledebate.jpg"}]
 
@@ -30,4 +35,4 @@ n(.split(slurp"picName.txt")"\n")
  if(d){
   d.title=wd[q]//+'n'+q
   d.alt=wd[q]}}"]
-[:footer"An "[:a{:href"https://dresdenlabs.appspot.com/"}"AlphaLabs"]" production. Images from "[:a{:href"https://dresdenlabs.appspot.com/iw/WikiPedia/Wikidata"}"WikiData"]"."]]])))
+[:footer"An "[:a{:href"https://dresdenlabs.appspot.com/"}"AlphaLabs"]" production. Images from "[:a{:href"https://dresdenlabs.appspot.com/iw/WikiPedia/Wikidata"}"WikiData"]"."]]]))))
