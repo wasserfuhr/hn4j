93f14ea8ea4c05aac1d093bc5c5fad07475ff63a
RaWa <wasserfuhr@yahoo.com>
2017-03-18 14:06:56 +0100

    ~WiDaJs.clj

--- WiDaJs.clj
+++ WiDaJs.clj
@@ -1,2 +1,2 @@
-(print(apply str(map(fn[l](let[i(.indexOf l" ")](str"'"(subs l 0(- i 1))"':'"(.trim(subs l(+ i 1)))"'\n")))
+(print(apply str(map(fn[l](let[i(.indexOf l" ")](str"'"(subs l 0(- i 1))"':'"(.trim(subs l(+ i 1)))"',\n")))
  (.split(slurp"https://floatingboat2013.appspot.com/rawHead/WiDaMan")"\n"))))
