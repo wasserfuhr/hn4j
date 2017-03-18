da041269241a21643879feea0e85e0c509d97317
RaWa <wasserfuhr@yahoo.com>
2017-03-18 21:32:05 +0100

    ~GetWpPic.clj

--- GetWpPic.clj
+++ GetWpPic.clj
@@ -19,7 +19,7 @@ w(Double.(get u 2))]
 ;
 ;https://floatingboat2013.appspot.com/WiDaPic?q=Q19837 =
 ;https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/245px-Steve_Jobs_Headshot_2010-CROP.jpg
-(str s0" "k" "w" "h"\n"))
+(str(format"%x %s %x %x"(Long.(subs s0 1)) k (Math/round w) (Math/round h))"\n"))
 ;(str s0" "k" "(Math/round h)"\n"))
 ;(str"<img id='"s0"'
 ;width="(Math/round(Math/min(*(/ h w)128)128.0))"
@@ -30,6 +30,12 @@ w(Double.(get u 2))]
 (catch Exception ex(str";"s0"e\n")))))
 (.split
 "Q23 Gilbert Stuart Williamstown Portrait of George Washington.jpg
-Q42 douglas adams portrait cropped.jpg"
+Q42 douglas adams portrait cropped.jpg
+Q1001 Portrait Gandhi.jpg
+Q1031 France Prešeren - lithograph (1866, Preširnove poezije) 01.jpg
+Q1035 Charles Darwin 1880.jpg
+Q1043 Carl von Linné.jpg
+Q1047 Bundesarchiv Bild 183-61849-0001, Indien, Otto Grotewohl bei Ministerpräsident Nehru cropped.jpg
+Q1048 Gaius Julius Caesar (100-44 BC).JPG"
 ;(slurp"picName.txt")
 "\n")))
