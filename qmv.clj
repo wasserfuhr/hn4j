cd89ab0c2bf78cfa7dcaaef78ceb7e98ed84435f
RaWa <wasserfuhr@yahoo.com>
2017-03-20 23:24:52 +0100

    ~qmv.clj

--- qmv.clj
+++ qmv.clj
@@ -1,2 +1,4 @@
-(print(apply str(map(fn[i](str"mv "(subs l 20)))
- (.split(slurp"qls")"\n"))))
+(print(apply str(map(fn[i](let[
+j(first(.split(subs l 20)"\\."))]
+(str"mv Q"j".json"j)));(format"%x"(Long.
+ (.split(slurp"qls")"\n")))))
