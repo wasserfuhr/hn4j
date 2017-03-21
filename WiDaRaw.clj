5c8a53fd17ba61867b02ce71ff52a37e6453635e
RaWa <wasserfuhr@yahoo.com>
2017-03-21 13:39:56 +0100

    ~WiDaRaw.clj

--- WiDaRaw.clj
+++ WiDaRaw.clj
@@ -1,4 +1,4 @@
 (map(fn[r](let[q(read-string(str"0x"r))]
 (if(not(.exists(java.io.File.(str"q/"r))))
  (spit(str"q/"r)(slurp(str"https://www.wikidata.org/wiki/Special:EntityData/Q"q".json"))))))
- (.split(slurp"https://floatingboat2013.appspot.com/eval/50955031")" "))
+ (.split(slurp"https://floatingboat2013.appspot.com/eval/58295043")" "))
