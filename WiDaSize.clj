2a4e2fa5705c1ed3150221bd7fbe2f79b57e0a6d
RaWa <wasserfuhr@yahoo.com>
2017-03-21 15:39:38 +0100

    ~WiDaSize.clj

--- WiDaSize.clj
+++ WiDaSize.clj
@@ -1,5 +1,2 @@
-(apply str(map(fn[l](str
-(first(.split(subs l 42)"\\."))
-))
-;(line-seq (java.io.BufferedReader. *in*)))))
- (.split *in* "\n")))
+(apply str(map(fn[l](str(first(.split(subs l 42)"\\."))))
+ (.split(slurp"qls")"\n")))
