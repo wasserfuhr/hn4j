5b12adb6962574a96e573299bdc11fc6f7a96ed4
RaWa <wasserfuhr@yahoo.com>
2017-03-21 08:28:09 +0100

    +cp nobel.clj nob.clj

---
+++ nob.clj
@@ -0,0 +1,45 @@
+(fn[rq rs](let[c(.getAttribute rq"c")v(.getAttribute rq"vars")t"AlphaHistory"
+s(.split(slurp"picSize.txt")"\n")
+n(.split(slurp"picName.txt")"\n")]
+(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title t" &laquo; &alpha;"];(:can c)(:css c)(:vp c)
+[:link{:rel"stylesheet":type"text/css":href"https://dresdenlabs.appspot.com/css"}]
+[:meta{:name"viewport":content"width=device-width,initial-scale=1.0"}]]
+[:body[:h1[:span#as[:a#aa{:href"/"}"&alpha;"]]"&raquo; "t]
+;[:script{:src"https://raw.githubusercontent.com/wasserfuhr/hn4j/master/WiDa.js"}]
+[:script{:src"https://rawgit.com/wasserfuhr/hn4j/master/WiDa.js"}]
+[:script"function cl(e,q){
+ var t=document.getElementById(q)
+ var x=e.pageX-t.offsetLeft
+ var y=e.pageY-t.offsetTop
+ if(y>200)
+  if(x>100){
+   t.width=t.width*1.2
+   t.height=t.height*1.2}
+  else{
+   t.width=t.width/1.2
+   t.height=t.height/1.2}
+ else
+  t.style.margin=(120-y)*t.width+'px 0 0'+(t.width/2-x)*t.width+'px'
+ console.log(q+': '+x+' '+y)}"]
+(map(fn[q](if(not(.startsWith q";"))(let[
+ d(read-string(str"0x"q))
+ p(filter(fn[i](.startsWith i(str d" ")))s)
+ s(filter(fn[i](.startsWith i(str"Q"d" ")))n)]
+ (if(and (not-empty p)(not-empty s)(not(.contains(first p)"  ")))
+ (let[a(.split(first p)" ")
+  bi(.indexOf(first s)" ")
+  b(subs(first s)bi)
+  h(read-string(str"0x"(get a 2)))
+  w(read-string(str"0x"(get a 3)))
+  e(java.net.URLEncoder/encode(.replaceAll b" ""_"))]
+[:div{:style"width:200px;height:240px;overflow:hidden;float:left;text-align:center"}
+ [:img{:id(str"i"q):width w :height h :title b :onclick(str"cl(event,'i"q"')")
+  :src(str"https://upload.wikimedia.org/wikipedia/commons/thumb/"
+   (subs (second a)0 1)"/"(second a)"/"(subs e 1)"/"w"px-"(subs e 1))}]])))))
+ (.split(slurp"https://floatingboat2013.appspot.com/eval/58295037")" "))
+[:script"for(var q in wd){
+ d=document.getElementById('r'+q)
+ if(d){
+  d.title=wd[q]//+'n'+q
+  d.alt=wd[q]}}"]
+[:footer"An "[:a{:href"https://dresdenlabs.appspot.com/"}"AlphaLabs"]" production. Images from "[:a{:href"https://dresdenlabs.appspot.com/iw/WikiPedia/Wikidata"}"WikiData"]"."]]]))))
