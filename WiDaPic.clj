5a0a054180c0b2a053b873ee7673ca4d503f0558
RaWa <wasserfuhr@yahoo.com>
2017-03-21 15:48:14 +0100

    ~WiDaPic.clj

--- WiDaPic.clj
+++ WiDaPic.clj
@@ -1,7 +1,7 @@
 (ns x(:require[clojure.data.json :as json]))
-(println(apply str(map(fn[r](let[
+(apply str(map(fn[r](let[
 q(read-string(str"0x"r))
 s(slurp(str"q/"r))]
  (str r" "(:value(:datavalue(:mainsnak(first(:P18 (:claims
   ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword)))))))))"\n")))
- (.split(slurp"https://floatingboat2013.appspot.com/eval/55905038")" "))))
+ (.split(slurp"qid")" ")))
