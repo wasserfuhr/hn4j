a31e6fe28bec7fc9c3207eddaeb026606e3a5902
RaWa <wasserfuhr@yahoo.com>
2017-03-22 22:45:56 +0100

    ~test.clj

--- test.clj
+++ test.clj
@@ -1,5 +1,5 @@
 (fn[rq rs](let[
- t(-(.getTime(java.util.Date.))(*(0x5608aa2b)1000))
+ t(-(.getTime(java.util.Date.))(* 0x5608aa2b 1000))
  d(long(/ t 1000.0))
  r(- t(* d 1000))]
  (spit(str"ad"(subs(format"%x" d)0 3)".log")
