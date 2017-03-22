429b718c309b9af4868faf602add5106b8170676
RaWa <wasserfuhr@yahoo.com>
2017-03-22 20:00:00 +0100

    ~test.clj

--- test.clj
+++ test.clj
@@ -1,8 +1,2 @@
 (fn[rq rs]
-;(spit "event.log" "test 1\n" :append true)
-(let[
- r(str(rand))
-  c(javax.servlet.http.Cookie."AlphaCookie"r)]
-   (.setMaxAge c(* 60 60 24 30))
-    (.addCookie rs c)
-     r))
+ (spit "event.log" "test 1\n" :append true))
