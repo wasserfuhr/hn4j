3eb6d77546cf5f020c74a3cd08844c6195edf543
RaWa <wasserfuhr@yahoo.com>
2017-03-23 00:17:59 +0100

    ~refCt.clj

--- refCt.clj
+++ refCt.clj
@@ -4,4 +4,4 @@ q(read-string(str"0x"r))
 s(slurp(str"q/"r))]
  (str r" "(count(keys(:labels
   ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword))))))"\n")))
- (.split(slurp"qid")" "))))
+ (.split(slurp"qidT")" "))))
