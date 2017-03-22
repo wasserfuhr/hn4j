477bfb5de00e79ae2e9dbf0b766d77d02c9f3dbd
RaWa <wasserfuhr@yahoo.com>
2017-03-22 18:40:02 +0100

    ~boot.clj

--- boot.clj
+++ boot.clj
@@ -15,8 +15,6 @@
 ;(.println *err*"\007")
 (println a i(subs uh 0 6)(if(=(.getScheme rq)"http")"h""s")(.getRequestURI rq))
 (if(not(.exists uf))(spit uf ua))
-(if(.startsWith(.getRequestURI rq)"/cookie")
- ((eval(read-string(slurp"cookie.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/favicon.ico")
   (.sendRedirect rs
    "https://upload.wikimedia.org/wikipedia/commons/a/a5/Greek_lc_alpha.png")
@@ -24,6 +22,10 @@
  ((eval(read-string(slurp"age.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/area")
  ((eval(read-string(slurp"area.clj")))rq rs)
+ (if(.startsWith(.getRequestURI rq)"/char")
+ (if(.startsWith(.getRequestURI rq)"/cookie")
+ ((eval(read-string(slurp"cookie.clj")))rq rs)
+ ((eval(read-string(slurp"char.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/math")
  ((eval(read-string(slurp"math.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/memoDev")
@@ -36,12 +38,8 @@
  ((eval(read-string(slurp"nob.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/phim")
  ((eval(read-string(slurp"phim.clj")))rq rs)
- (if(.startsWith(.getRequestURI rq)"/rq")
-  (slurp"/root/rq.zip")
-  (if(.startsWith(.getRequestURI rq)"/tap")
+ (if(.startsWith(.getRequestURI rq)"/tap")
  ((eval(read-string(slurp"tapestry.clj")))rq rs)
- (if(.startsWith(.getRequestURI rq)"/char")
- ((eval(read-string(slurp"char.clj")))rq rs)
  (if(.startsWith(.getRequestURI rq)"/a9e")
   (.sendRedirect rs
    "https://upload.wikimedia.org/wikipedia/commons/6/6b/Gordon_Moore.jpg")
@@ -49,10 +47,10 @@
 [:body
 [:p"Hi "(subs uh 0 6)"@"i]
 "&alpha;t"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
-[:br]Ttry "
+[:br][:br]"Try "
 [:a{:href"https://i.sl4.eu/age"}"History"]" "
 [:a{:href"https://i.sl4.eu/math"}"Math"]" "
 [:a{:href"https://i.sl4.eu/memo"}"Memory"]" "
 [:a{:href"https://i.sl4.eu/phim"}"Physics"]" "
 [:a{:href"https://i.sl4.eu/tap"}"Tapestry"]
-]])))))))))))))))))
+]]))))))))))))))))
