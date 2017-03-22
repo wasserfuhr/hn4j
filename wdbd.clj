3dae4ee15558cc50b8c2cc6a32a9337fec0fd1c9
RaWa <wasserfuhr@yahoo.com>
2017-03-22 14:44:53 +0100

    ~wdbd.clj

--- wdbd.clj
+++ wdbd.clj
@@ -1,4 +1,5 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[m"03"d"22"](map(fn[i]i)
+(let[m"03"d"22"](map(fn[i](subs(:value(:e i))28))
+;"http://www.wikidata.org/entity/Q..."
 (:bindings(:results
  (json/read-str (slurp(str"wd/"m d)) :key-fn keyword)))))
