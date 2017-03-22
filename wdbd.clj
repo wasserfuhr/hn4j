8761ca301d9e4a478688e61958aa8896c2dbc1ef
RaWa <wasserfuhr@yahoo.com>
2017-03-22 14:43:02 +0100

    ~wdbd.clj

--- wdbd.clj
+++ wdbd.clj
@@ -1,3 +1,4 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[m"03"d"22](first(:bindings(:results
- (json/read-str (slurp(str"wd/"m d)) :key-fn keyword))))
+(let[m"03"d"22"](map(fn[i]i)
+(:bindings(:results
+ (json/read-str (slurp(str"wd/"m d)) :key-fn keyword)))))
