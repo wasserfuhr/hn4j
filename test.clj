d972148ba89db6281c6028652ae6c3429a6f637f
RaWa <wasserfuhr@yahoo.com>
2017-03-22 20:06:29 +0100

    ~test.clj

--- test.clj
+++ test.clj
@@ -1,8 +1,6 @@
-(fn[rq rs]
- (spit "event.log" "test 1\n" :append true))
-
 (fn[rq rs](let[
  t(-(.getTime(java.util.Date.))1443408427000)
   d(long(/ t 1000.0))
-   r(- t(* d 1000))];mod?
+   r(- t(* d 1000))]
+    (spit "event.log" "test 1\n" :append true))
     (format"%02x.%02x"d(quot(* 256 r)1000))))
