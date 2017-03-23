ec4fd9b95eef550d1f17c9b3fe9acc620e870afb
RaWa <wasserfuhr@yahoo.com>
2017-03-23 15:33:41 +0100

    ~phimDev.clj

--- phimDev.clj
+++ phimDev.clj
@@ -5,48 +5,47 @@ n(.split(slurp"qpic")"\n")]
 [:link{:rel"stylesheet":type"text/css":href"https://dresdenlabs.appspot.com/css"}]
 [:meta{:name"viewport":content"width=device-width,initial-scale=1.0"}]]
 [:body[:h1{:style"background-color:#c0dd00"}[:span#as[:a#aa{:href"/"}"&alpha;"]]"&raquo; "t]
-;[:script{:src"https://raw.githubusercontent.com/wasserfuhr/hn4j/master/WiDa.js"}]
 [:script{:src"https://rawgit.com/wasserfuhr/hn4j/master/WiDa.js"}]
 [:script"
  document.getElementById('moreDiv').style.display=(more>0)?'block':'none'
-  more--
-   if(more==0){
-     var c='ABEGJOSPW'
-       document.getElementById('l').innerHTML=c.charAt(Math.floor(Math.random()*c.length))}
+ more--
+ if(more==0){
+  var c='ABEGJOSPW'
+  document.getElementById('l').innerHTML=c.charAt(Math.floor(Math.random()*c.length))}
 
  for(var k in ps){
-   document.getElementById('t'+k).innerHTML=ps[k]
-     document.getElementById('t'+k).style.display=(more>0)?'block':'none'
-       if(more==0)
-          document.getElementById(k).style.borderColor='#fff'}
-	   document.getElementById('more').innerHTML=more},1000);
+  document.getElementById('t'+k).innerHTML=ps[k]
+  document.getElementById('t'+k).style.display=(more>0)?'block':'none'
+  if(more==0)
+   document.getElementById(k).style.borderColor='#fff'}
+  document.getElementById('more').innerHTML=more},1000)
 
 function d(i){
  l=document.getElementById('l').innerHTML
-  s=document.getElementById(i).style
-   s.borderColor=('yellow'==s.borderColor)?'#fff':'yellow'
-    xhr=new XMLHttpRequest()
-     try{
-       xhr.open('get','/char/'+l+'/'+i)
-         xhr.onreadystatechange=function(){
-	    if(xhr.readyState===4)
-	        if(xhr.status===200)
-		     h=xhr.responseText.split('.')
-		         else
-			      console.log('Error: '+xhr.status)}
-			        xhr.send(null)}
-				 catch(e){console.log('Error: '+xhr.status)}}
+ s=document.getElementById(i).style
+ s.borderColor=('yellow'==s.borderColor)?'#fff':'yellow'
+ xhr=new XMLHttpRequest()
+ try{
+  xhr.open('get','/char/'+l+'/'+i)
+  xhr.onreadystatechange=function(){
+  if(xhr.readyState===4)
+   if(xhr.status===200)
+    h=xhr.responseText.split('.')
+   else
+    console.log('Error: '+xhr.status)}
+   xhr.send(null)}
+ catch(e){console.log('Error: '+xhr.status)}}
 
 function q(){try{for(var k in ps){
-  var l=document.getElementById('l').innerHTML
-    var s=document.getElementById(k).style
-      s.borderColor=('yellow'==s.borderColor)==m(l,ps[k])?'green':'red'
-        if('green'==s.borderColor)hc++}
-	 hm+=6;
-	  document.getElementById('hc').innerHTML=hc;
-	   document.getElementById('hm').innerHTML=hm;
-	    more=5
-	    }catch(e){alert(e)}}
+ var l=document.getElementById('l').innerHTML
+ var s=document.getElementById(k).style
+ s.borderColor=('yellow'==s.borderColor)==m(l,ps[k])?'green':'red'
+  if('green'==s.borderColor)hc++}
+  hm+=6;
+  document.getElementById('hc').innerHTML=hc;
+  document.getElementById('hm').innerHTML=hm;
+  more=5
+  }catch(e){alert(e)}}
 
 var p=["(apply str(map(fn[q](str"'"(second q)"',\n"))x))"]
 function abc(l){for(i=0;i<p.length;i++)
