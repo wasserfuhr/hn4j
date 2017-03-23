4de08f035d0fe1f8b2f31f855c935f58fd88426c
RaWa <wasserfuhr@yahoo.com>
2017-03-23 15:35:44 +0100

    ~phimDev.clj

--- phimDev.clj
+++ phimDev.clj
@@ -1,6 +1,8 @@
 (fn[rq rs](let[c(.getAttribute rq"c")v(.getAttribute rq"vars")t"&Phi;Memory"
 s(.split(slurp"picSize.txt")"\n")
-n(.split(slurp"qpic")"\n")]
+n(.split(slurp"qpic")"\n")
+w(.split(slurp(str"https://floatingboat2013.appspot.com/rawHead/MemoPic"))"\n")
+x(map #(.split(.trim %)" ")w)]
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title t" &laquo; &alpha;"];(:can c)(:css c)(:vp c)
 [:link{:rel"stylesheet":type"text/css":href"https://dresdenlabs.appspot.com/css"}]
 [:meta{:name"viewport":content"width=device-width,initial-scale=1.0"}]]
