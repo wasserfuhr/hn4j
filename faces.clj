42cf30662a8460f6e77ef8ba8040759e2669f5d5
RaWa <wasserfuhr@yahoo.com>
2017-03-24 20:34:58 +0100

    ~faces.clj

--- faces.clj
+++ faces.clj
@@ -39,7 +39,7 @@ n(.split(slurp"qpic")"\n")]
     [:img{:id(str"i"q):width w :height h :onclick(str"cl(event,'i"q"')")
      :src(str"https://upload.wikimedia.org/wikipedia/commons/thumb/"
      (subs (second a)0 1)"/"(second a)"/"(subs e 1)"/"w"px-"(subs e 1))}]]
-    [:br][:input{:type"button":value"?":onclick"alert('"e"')"}]])))))
+    [:br][:input{:type"button":value"?":onclick(str"alert('"e"')")}]])))))
       (.split"17 4c"" "))
    ;    (.split(slurp"qref")" "))
 [:br{:style"clear:both"}]
