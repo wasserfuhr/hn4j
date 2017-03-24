326636bd171dfd59173ad53b755456ed300509a4
RaWa <wasserfuhr@yahoo.com>
2017-03-24 21:58:17 +0100

    ~boot.clj

--- boot.clj
+++ boot.clj
@@ -57,16 +57,4 @@ h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
  (if(.startsWith(.getRequestURI rq)"/a9e")
   (.sendRedirect rs
    "https://upload.wikimedia.org/wikipedia/commons/6/6b/Gordon_Moore.jpg")
-    ((eval(read-string(slurp"index.clj")))rq rs)
-;(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
-[:body
-[:p"Hi "(subs uh 0 6)"@"i]
-"&alpha;t"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
-[:br][:br]"Try "
-[:a{:href"https://i.sl4.eu/age"}"History"]" "
-[:a{:href"https://i.sl4.eu/math"}"Math"]" "
-[:a{:href"https://i.sl4.eu/memo"}"Memory"]" "
-[:a{:href"https://i.sl4.eu/phim"}"Physics"]" "
-[:a{:href"https://i.sl4.eu/tap"}"Tapestry"]
-]])
-))))))))))))))))))))
+    ((eval(read-string(slurp"index.clj")))rq rs)))))))))))))))))))))
