3446d7e59ac6ec7a78bfe6ea14110bf4a5a91282
RaWa <wasserfuhr@yahoo.com>
2017-03-24 22:01:10 +0100

    ~index.clj

--- index.clj
+++ index.clj
@@ -1,15 +1,16 @@
 (fn[rq rs](let[
-
-    ((eval(read-string(slurp"index.clj")))rq rs)
-;(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
-[:body
-[:p"Hi "(subs uh 0 6)"@"i]
+ h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
+  (. h update m)(.digest h)))
+ f(fn[h](apply str(map
+  #(format "%02x"(bit-and % 0xff))h)))
+ ua(.getHeader rq"User-Agent")
+ uh(f(h(.getBytes ua)))]
+(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]][:body
+[:p"Hi "(subs uh 0 6)"@i"]
 "&alpha;t"a": we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
 [:br][:br]"Try "
 [:a{:href"https://i.sl4.eu/age"}"History"]" "
 [:a{:href"https://i.sl4.eu/math"}"Math"]" "
 [:a{:href"https://i.sl4.eu/memo"}"Memory"]" "
 [:a{:href"https://i.sl4.eu/phim"}"Physics"]" "
-[:a{:href"https://i.sl4.eu/tap"}"Tapestry"]
-]])
-))))))))))))))))))))
+[:a{:href"https://i.sl4.eu/tap"}"Tapestry"]]])))
