1f9edbb421bf8649551348416404b53a7a0be0cf
RaWa <wasserfuhr@yahoo.com>
2017-03-24 16:22:48 +0100

    ~wfn.clj

--- wfn.clj
+++ wfn.clj
@@ -1,5 +1,5 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[q"https://query.wikidata.org/sparql?query=select%3Fp%3Fi%20where%7B%3Fp%20wdt%3AP2020%3Fi%7Dlimit30000\&format=json"]
+(let[q"https://query.wikidata.org/sparql?query=select%3Fp%3Fi%20where%7B%3Fp%20wdt%3AP2020%3Fi%7Dlimit30000&format=json"]
 (spit"wf"(.trim(apply str
  (map(fn[i](format"%x %s"(Long.(subs(:value(:p i))32))(:value(:i i))))
  ;"http://www.wikidata.org/entity/Q..."
