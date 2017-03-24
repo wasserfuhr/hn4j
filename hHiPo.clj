14c25b35a0edd4268fcde089a767477e66895455
RaWa <wasserfuhr@yahoo.com>
2017-03-24 19:11:59 +0100

    ~hHiPo.clj

--- hHiPo.clj
+++ hHiPo.clj
@@ -1,1 +1,1 @@
-(apply str(map(fn[l](str(first(.split l" "))" "))(.split(slurp"wfn.txt")"\n")))
+(apply str(map(fn[l](let[m(.split(subs l 45)"/")](format"%s %s\n")(read-string(str"0x"(first m)))(second m)))(.split(slurp"hHiPo")"\n")))
