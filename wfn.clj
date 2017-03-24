c3d0f587a57e045680e5924427fc479f6c9d67de
RaWa <wasserfuhr@yahoo.com>
2017-03-24 16:20:20 +0100

    ~wfn.clj

--- wfn.clj
+++ wfn.clj
@@ -1,8 +1,6 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[
-q"https://query.wikidata.org/sparql?query=select%3Fe%20where%7B%3Fe%20wdt%3AP31%20wd%3AQ5.%3Fe%20wdt%3AP569%20%3Fd.filter%28MONTH%28%3Fd%29%3D"m"%26%26DAY%28%3Fd%29%3D"d"%29%7Dlimit%2020000&format=json"
-]
+(let[q"https://query.wikidata.org/sparql?query=select%3Fp%3Fi%20where%7B%3Fp%20wdt%3AP2020%3Fi%7Dlimit30000\&format=json"]
 (spit"wf"(.trim(apply str
- (map(fn[i](format"%x %s"(Long.(subs(:value(:p i))32))(:i i)))
+ (map(fn[i](format"%x %s"(Long.(subs(:value(:p i))32))(:value(:i i))))
  ;"http://www.wikidata.org/entity/Q..."
   (:bindings(:results(json/read-str (slurp "wfn") :key-fn keyword))))))))
