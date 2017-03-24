07b5b5ad2b3c2414292157d036c028461d569e1f
RaWa <wasserfuhr@yahoo.com>
2017-03-24 16:18:40 +0100

    ~wfn.clj

--- wfn.clj
+++ wfn.clj
@@ -3,6 +3,6 @@
 q"https://query.wikidata.org/sparql?query=select%3Fe%20where%7B%3Fe%20wdt%3AP31%20wd%3AQ5.%3Fe%20wdt%3AP569%20%3Fd.filter%28MONTH%28%3Fd%29%3D"m"%26%26DAY%28%3Fd%29%3D"d"%29%7Dlimit%2020000&format=json"
 ]
 (spit"wf"(.trim(apply str
- (map(fn[i](format"%x %s"(Long.(subs(:value(:e i))32))))
+ (map(fn[i](format"%x %s"(Long.(subs(:value(:p i))32))(:i i)))
  ;"http://www.wikidata.org/entity/Q..."
   (:bindings(:results(json/read-str (slurp "wfn") :key-fn keyword))))))))
