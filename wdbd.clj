76c0c17177760895ea519e16a860197e6287f4ce
RaWa <wasserfuhr@yahoo.com>
2017-03-25 16:04:53 +0100

    ~wdbd.clj

--- wdbd.clj
+++ wdbd.clj
@@ -1,5 +1,5 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[m"03"d"25"
+(let[m"03"d"26"
 q(str"https://query.wikidata.org/sparql?query=select%3Fe%20where%7B%3Fe%20wdt%3AP31%20wd%3AQ5.%3Fe%20wdt%3AP569%20%3Fd.filter%28MONTH%28%3Fd%29%3D"m"%26%26DAY%28%3Fd%29%3D"d"%29%7Dlimit%2020000&format=json")
 ]
 (spit(str"wd/"m d)(.trim(apply str
