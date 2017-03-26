614f3b15aab9238a99ef9c6bb2c6b873a98a347f
RaWa <wasserfuhr@yahoo.com>
2017-03-26 23:40:20 +0200

    ~isa.clj

--- isa.clj
+++ isa.clj
@@ -1,9 +1,11 @@
 (ns x(:require[clojure.data.json :as json]))
 
-(map(fn[f](let[
+(print(apply str(map(fn[f](let[
 l(.getName f)
 q(read-string(str"0x"l))
 s(slurp(str"q/"l))]
- (print(format"%s %s\n"l(:value(:datavalue(:mainsnak(first(:P31(:claims
-   ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword)))))))))))))
-   (.listFiles(java.io.File."q")))
+ (format"%s %s\n"
+  (:id(:value(:datavalue(:mainsnak(first(:P31(:claims
+   ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword))))))))))
+  l)))
+ (.listFiles(java.io.File."q")))))
