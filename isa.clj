7253054f1f17270c5af2e33b43cb52e956607c57
RaWa <wasserfuhr@yahoo.com>
2017-03-26 23:35:00 +0200

    ~isa.clj

--- isa.clj
+++ isa.clj
@@ -5,6 +5,6 @@ l(.getName f)
 n(subs l 2)
 q(read-string(str"0x"n))
 s(slurp(str"q/"l))]
- (print(format"%s %s\n"n(:value(:datavalue(:mainsnak(first(:P18(:claims
+ (print(format"%s %s\n"l(:value(:datavalue(:mainsnak(first(:P18(:claims
    ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword)))))))))))))
    (.listFiles(java.io.File."q")))
