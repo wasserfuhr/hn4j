68d9e9ba76398a38e4637fa6ed40509805584240
RaWa <wasserfuhr@yahoo.com>
2017-03-26 20:33:10 +0200

    ~ls.clj

--- ls.clj
+++ ls.clj
@@ -1,2 +1,2 @@
 (print(apply str(map(fn[l](let[f(java.io.File l)](format"%x %x %s\n"
-(.lastModied f)(.length f)(subs f 2))))(.listFiles(java.io.File."q")))))
+(.lastModified f)(.length f)(subs f 2))))(.listFiles(java.io.File."q")))))
