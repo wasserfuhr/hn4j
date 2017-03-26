1667d233ac29a9ab219d89e789be1daad640173c
RaWa <wasserfuhr@yahoo.com>
2017-03-27 00:49:43 +0200

    ~refCt.clj

--- refCt.clj
+++ refCt.clj
@@ -3,10 +3,6 @@
 l(.getName f)
 q(read-string(str"0x"l))
 s(slurp(str"q/"l))]
-q(read-string(str"0x"r))
-s(slurp(str"q/"r))]
  (str r" "(count(keys(:labels
   ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword))))))"\n")))
- (.split(slurp"qidT")" ")
- (.listFiles(java.io.File."q"))
-)))
+ (.listFiles(java.io.File."q")))))
