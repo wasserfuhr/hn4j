513030c6acb6ec681ef5a1275752e91c23119f4c
RaWa <wasserfuhr@yahoo.com>
2017-03-26 22:19:27 +0200

    ~WiDaPic.clj

--- WiDaPic.clj
+++ WiDaPic.clj
@@ -4,4 +4,4 @@ q(read-string(str"0x"r))
 s(slurp(str"q/"r))]
  (str r" "(:value(:datavalue(:mainsnak(first(:P18 (:claims
   ((keyword (str"Q"q))(:entities(json/read-str s :key-fn keyword)))))))))"\n")))
- (.split(slurp"qid")" "))))
+ (.split(slurp"q163700.txt")" "))))
