a0f357784b6d9eaceef3a899c4074685f82497ca
RaWa <wasserfuhr@yahoo.com>
2017-03-28 18:40:23 +0200

    ~ql.clj

--- ql.clj
+++ ql.clj
@@ -2,7 +2,8 @@
 (let[
  ;"http://www.wikidata.org/entity/Q..."
 e(java.net.URLEncoder/encode
- "select?h?i where{?h wdt:P166 wd:Q10905276;wdt:P31 wd:Q5;wdt:P18?i}limit2000")
+ "select?h?i where{?h wdt:P166 wd:Q10905380;wdt:P31 wd:Q5;wdt:P18?i}limit2000")
+;"select?h?i where{?h wdt:P166 wd:Q10905276;wdt:P31 wd:Q5;wdt:P18?i}limit2000")
 ;"select?h?i where{?h wdt:P31 wd:Q5;wdt:P166 wd:Q163700;wdt:P18?i}limit 2000")
 q(str"https://query.wikidata.org/sparql?query="e"&format=json")]
 (print(apply str
