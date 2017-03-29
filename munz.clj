10c6d2329c4a7ff0e5921a66be73a2fddee1f08e
RaWa <wasserfuhr@yahoo.com>
2017-03-29 13:52:08 +0200

    ~munz.clj

--- munz.clj
+++ munz.clj
@@ -2,7 +2,7 @@
 (let[
 l(count"http://www.wikidata.org/entity/Q")
 e(java.net.URLEncoder/encode
- "select?h?i where{?h wdt:P1284 ?i;wdt:P31 wd:Q5}limit20")
+ "select?h?i where{?h wdt:P1284 ?i;wdt:P31 wd:Q5}limit20000")
 q(str"https://query.wikidata.org/sparql?query="e"&format=json")]
 (print(apply str
  (map(fn[i](format"%x %x\n"
