0022b08a400f50670e5cc9892175e70ee4c70163
RaWa <wasserfuhr@yahoo.com>
2017-03-29 13:48:06 +0200

    ~munz.clj

--- munz.clj
+++ munz.clj
@@ -5,5 +5,5 @@ e(java.net.URLEncoder/encode
  "select?h?i where{?h wdt:P1284 ?i;wdt:P31 wd:Q5}limit20")
 q(str"https://query.wikidata.org/sparql?query="e"&format=json")]
 (print(apply str
- (map(fn[i](format"%x %s\n"(Long.(subs(:value(:h i))l)(Long.(:i)))))
+ (map(fn[i](format"%x %s\n"(Long.(subs(:value(:h i))l)(Long.(:value(:i i))))))
  (:bindings(:results(json/read-str (slurp q) :key-fn keyword)))))))
