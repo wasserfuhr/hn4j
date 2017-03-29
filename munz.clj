d1740d8d52742b2e59778c6ab59488409d368a97
RaWa <wasserfuhr@yahoo.com>
2017-03-29 13:47:24 +0200

    ~munz.clj

--- munz.clj
+++ munz.clj
@@ -2,10 +2,8 @@
 (let[
 l(count"http://www.wikidata.org/entity/Q")
 e(java.net.URLEncoder/encode
- "select?h?i where{?h wdt:P166 wd:Q10905380;wdt:P31 wd:Q5;wdt:P18?i}limit2000")
-;"select?h?i where{?h wdt:P166 wd:Q10905276;wdt:P31 wd:Q5;wdt:P18?i}limit2000")
-;"select?h?i where{?h wdt:P31 wd:Q5;wdt:P166 wd:Q163700;wdt:P18?i}limit 2000")
+ "select?h?i where{?h wdt:P1284 ?i;wdt:P31 wd:Q5}limit20")
 q(str"https://query.wikidata.org/sparql?query="e"&format=json")]
 (print(apply str
- (map(fn[i](format"%x "(Long.(subs(:value(:h i))l))))
+ (map(fn[i](format"%x %s\n"(Long.(subs(:value(:h i))l)(Long.(:i)))))
  (:bindings(:results(json/read-str (slurp q) :key-fn keyword)))))))
