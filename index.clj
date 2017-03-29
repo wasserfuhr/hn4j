99e10838fff125e6b02672d2820e67a607b7efa1
RaWa <wasserfuhr@yahoo.com>
2017-03-29 16:42:43 +0200

    ~index.clj

--- index.clj
+++ index.clj
@@ -9,7 +9,8 @@
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]][:body
 [:div{:style"overflow:hidden;height:60px;width:750px"}
 [:div#rot{:style"overflow:hidden;height:60px;width:900px;margin-left:-50px"}
- [:img{:style"height:60px"}]
+ [:img{:style"height:60px"
+ :src"https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Susan_Polgar_6.jpg/192px-Susan_Polgar_6.jpg"}]
 ;(map(fn[i][:img{:id(str"i"i):alt i}])l)
 ]]
 [:p"Hi "(subs uh 0 6)"@i"]
