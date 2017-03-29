1a6a07ad12d4558896e00bb63bd20f9dcb4d9abc
RaWa <wasserfuhr@yahoo.com>
2017-03-29 15:02:32 +0200

    ~WiDaRaw.clj

--- WiDaRaw.clj
+++ WiDaRaw.clj
@@ -1,4 +1,4 @@
 (map(fn[r](let[q(read-string(str"0x"r))]
 (if(not(.exists(java.io.File.(str"q/"r))))
  (spit(str"q/"r)(slurp(str"https://www.wikidata.org/wiki/Special:EntityData/Q"q".json"))))))
- (.split(slurp"q10905380.txt")" "))
+ (.split(slurp"munzId.txt")" "))
