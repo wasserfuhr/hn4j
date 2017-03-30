d7c9f05ba669ddc864656bfdd2bad5ea88350405
RaWa <wasserfuhr@yahoo.com>
2017-03-30 22:14:34 +0200

    ~wdName.clj

--- wdName.clj
+++ wdName.clj
@@ -6,5 +6,5 @@ s(slurp(str"q/"r))]
  (str r" "(:value(:datavalue(:mainsnak(first(:P18(:claims
   ((keyword(str"Q"q))(:entities(json/read-str s :key-fn keyword)))))))))"\n")))
 ; (.listFiles(java.io.File."q"))
- (.split(slurp"m330")"\n")
+ (.split(slurp"m33")"\n")
 )))
