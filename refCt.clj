f17ce2254fa9ad199a24ef3e28e672f49039d5d5
RaWa <wasserfuhr@yahoo.com>
2017-03-30 21:54:55 +0200

    ~refCt.clj

--- refCt.clj
+++ refCt.clj
@@ -1,8 +1,9 @@
 (ns x(:require[clojure.data.json :as json]))
 (print(apply str(map(fn[f](let[
-l(.getName f)
+l f;(.getName f)
 q(read-string(str"0x"l))
 s(slurp(str"q/"l))]
  (str l" "(count(keys(:labels
   ((keyword(str"Q"q))(:entities(json/read-str s :key-fn keyword))))))"\n")))
- (.listFiles(java.io.File."q")))))
+; (.listFiles(java.io.File."q")))))
+  (.split(slurp"wd/0330")" ")))))
