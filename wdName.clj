a339abd3e02e5ca61f1fef650b3f4236891c2b4e
RaWa <wasserfuhr@yahoo.com>
2017-03-30 22:17:06 +0200

    ~wdName.clj

--- wdName.clj
+++ wdName.clj
@@ -1,8 +1,8 @@
 (ns x(:require[clojure.data.json :as json]))
 (print(apply str(map(fn[f](let[
 r(.split f" ");(.getName f)
-q(read-string(str"0x"r))
-s(slurp(str"q/"r))]
+q(read-string(str"0x"(first r)))
+s(slurp(str"q/"(first r)))]
  (str r" "(:value(:datavalue(:mainsnak(first(:P18(:claims
   ((keyword(str"Q"q))(:entities(json/read-str s :key-fn keyword)))))))))"\n")))
 ; (.listFiles(java.io.File."q"))
