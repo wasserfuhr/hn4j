4b4c28ac4031bae67d8fcd122f3d98bb03a5e5db
RaWa <wasserfuhr@yahoo.com>
2017-03-30 23:23:21 +0200

    ~WiDaRaw.clj

--- WiDaRaw.clj
+++ WiDaRaw.clj
@@ -1,4 +1,4 @@
 (map(fn[r](let[q(read-string(str"0x"r))]
 (if(not(.exists(java.io.File.(str"q/"r))))
  (spit(str"q/"r)(slurp(str"https://www.wikidata.org/wiki/Special:EntityData/Q"q".json"))))))
- (.split(slurp"wd/0330")" "))
+ (.split(slurp"wd/0331")" "))
