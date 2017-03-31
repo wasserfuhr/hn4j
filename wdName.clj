40152ddbb9138904055e1e58aa0378ac899b61cc
RaWa <wasserfuhr@yahoo.com>
2017-03-31 13:25:16 +0200

    ~wdName.clj

--- wdName.clj
+++ wdName.clj
@@ -6,7 +6,7 @@ q(read-string(str"0x"t))
 s(slurp(str"q/"t))]
 ; (str t" "(:value(:en(:labels
 ;  (str t" "(count(:sitelinks
-; (str t" "(:value(:en(:descriptions
+ (str t" "(:value(:en(:descriptions
  (str t" "(:value(:en(:labels
   ((keyword(str"Q"q))(:entities(json/read-str s :key-fn keyword))
   ))))"\n")))
