45493c358acac8b78d619869a17f85e2985effcd
RaWa <wasserfuhr@yahoo.com>
2017-03-31 20:03:56 +0200

    ~pcurl.clj

--- pcurl.clj
+++ pcurl.clj
@@ -2,5 +2,5 @@
  d(.lastIndexOf l".")
  m(subs l d)
  n(first(.split(subs l 5)"/"))]
- (str"curl https://upload.wikimedia.org/wikipedia/commons/thumb/"(subs l 0 4)"/"n"/64px-"n" > tpic;a=`sha256sum tpic`;b=${a:0:64};mv tpic p/$b."m"\n")))
+ (str"curl https://upload.wikimedia.org/wikipedia/commons/thumb/"(subs l 0 4)"/"n"/64px-"n" > tpic;a=`sha256sum tpic`;b=${a:0:64};mv tpic p/$b"m"\n")))
  (.split(slurp"urPic.txt")"\n"))))
