439ec082ce51a69ebf188578bcf41d146e123b3a
RaWa <wasserfuhr@yahoo.com>
2017-03-31 18:22:46 +0200

    ~pcurl.clj

--- pcurl.clj
+++ pcurl.clj
@@ -1,13 +1,1 @@
-(fn[rq rs](let[
- h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
-  (. h update m)(.digest h)))
- f(fn[h](apply str(map
-  #(format "%02x"(bit-and % 0xff))h)))
-  
-
-
-var now=new Date().gesetInterval(function(){
-try{
- ct++
- //if(ct<2)alert(1)
-(slurp'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/192px-President_Barack_Obama.jpg")
+(.split(slurp"hilde.js")"\n")
