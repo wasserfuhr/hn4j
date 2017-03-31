784d5e6ba66ba402ba4ccaaac531fa1b89752a56
RaWa <wasserfuhr@yahoo.com>
2017-03-31 18:34:33 +0200

    ~pcurl.clj

--- pcurl.clj
+++ pcurl.clj
@@ -1,4 +1,4 @@
 (print(apply str(map(fn[l](let[
  n(first(.split(subs l 6)"/"))]
- (str"curl https://upload.wikimedia.org/wikipedia/commons/thumb"(subs l 1 5)n"/64-px/"n"|wc\n")))
+ (str"curl https://upload.wikimedia.org/wikipedia/commons/thumb/"(subs l 1 5)n"/64-px/"n"|wc\n")))
  (.split(slurp"urPic.txt")"\n"))))
