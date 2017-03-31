3dd87f597cf7048e7550054a743a4ec03ca38ce2
RaWa <wasserfuhr@yahoo.com>
2017-03-31 13:32:42 +0200

    ~wdName.clj

--- wdName.clj
+++ wdName.clj
@@ -6,8 +6,8 @@ q(read-string(str"0x"t))
 s(slurp(str"q/"t))]
 ; (str t" "(:value(:en(:labels
 ;  (str t" "(count(:sitelinks
- (str t" "(:value(:en(:descriptions
+; (str t" "(:value(:en(:descriptions
  (str t" "(:value(:en(:labels
   ((keyword(str"Q"q))(:entities(json/read-str s :key-fn keyword))
   ))))"\n")))
- (.split(slurp"m330")"\n"))))
+ (.split(slurp"refCt.txt")"\n"))))
