c337183782ee09e0bb2c888d4fa9d36cc8a6321c
RaWa <wasserfuhr@yahoo.com>
2017-04-01 18:21:57 +0200

    ~wdbd.clj

--- wdbd.clj
+++ wdbd.clj
@@ -1,8 +1,9 @@
 (ns x(:require[clojure.data.json :as json]))
 (let[m"04"d"01"
 l(count"http://www.wikidata.org/entity/Q")
-q(str"https://query.wikidata.org/sparql?query=select%3Fe%20where%7B%3Fe%20wdt%3AP31%20wd%3AQ5.%3Fe%20wdt%3AP569%20%3Fd.filter%28MONTH%28%3Fd%29%3D"m"%26%26DAY%28%3Fd%29%3D"d"%29%7Dlimit%2020000&format=json")
-]
+e(java.net.URLEncoder/encode
+ "select?e where{?e wdt:P31 wd:Q5.?e wdt:P569 ?d.filter(MONTH(?d)="m"&&DAY(?d)="d")}limit 20000")
+q(str"https://query.wikidata.org/sparql?query="e"&format=json")]]
 (spit(str"wd/"m d)(.trim(apply str
  (map(fn[i](format"%x "(Long.(subs(:value(:e i))l))))
   (:bindings(:results(json/read-str (slurp q) :key-fn keyword))))))))
