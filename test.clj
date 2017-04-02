c877b1ec6b349230ef80e792141bafaa0e5b48d8
RaWa <wasserfuhr@yahoo.com>
2017-04-02 22:36:04 +0200

    ~test.clj

--- test.clj
+++ test.clj
@@ -2,5 +2,7 @@
  t(-(.getTime(java.util.Date.))(* 0x5608aa2b 1000))
  d(long(/ t 1000.0))
  r(- t(* d 1000))]
- (spit(str"ad"(subs(format"%x" d)0 3)".log")
-  (format"%x.%02x %s\n"d(quot(* 256 r)1000) "test") :append true)))
+ ;(spit(str"ad"(subs(format"%x" d)0 3)".log")
+  (format"%x.%02x %s\n"d(quot(* 256 r)1000) "test")
+  ;:append true)
+  ))
