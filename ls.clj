ced35bfffa3a2e41a6402e95bb63fb0f2f45d26b
RaWa <wasserfuhr@yahoo.com>
2017-04-03 00:20:02 +0200

    ~ls.clj

--- ls.clj
+++ ls.clj
@@ -1,4 +1,6 @@
 (fn[rq rs]
 (str"[\n"(apply str(map(fn[f](let[l(.getName f)](format"\"%x %05x %s\",\n"
 (-(/(.lastModified f)1000)0x5608aa2b)
-(.length f)l)))(.listFiles(java.io.File."a"))))"]"))
+(.length f)l)))
+(sort-by #(.lastModified %)(.listFiles(java.io.File."a"))
+)))"]"))
