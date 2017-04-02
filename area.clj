af0f15141704ce74fdb25a037df30dd5c50cfba3
RaWa <wasserfuhr@yahoo.com>
2017-04-02 22:19:53 +0200

    ~area.clj

--- area.clj
+++ area.clj
@@ -1,7 +1,7 @@
 (fn[rq rs]
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"α β"]]
 [:body
-[:p"Hi "(subs uh 0 6)"@i"]
+[:p"Hi (subs uh 0 6)@i"]
 "&alpha;ta: we are "[:a{:href"https://dresdenlabs.appspot.com/"}"&alpha;"]" approaching &beta;."
 "create our future yourself:"[:br]
 [:textarea#t{:cols 32 :rows 32}"This TextArea is stored in a BlockChain!
