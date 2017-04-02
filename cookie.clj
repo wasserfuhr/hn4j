a5218466e13ac3e73602fbca3fc115c6ef1ed31d
RaWa <wasserfuhr@yahoo.com>
2017-04-02 18:54:24 +0200

    ~cookie.clj

--- cookie.clj
+++ cookie.clj
@@ -1,6 +1,5 @@
 (fn[rq rs](let[
- r(str(rand))
-  c(javax.servlet.http.Cookie."AlphaCookie"r)]
-   (.setMaxAge c(* 60 60 24 30))
-    (.addCookie rs c)
-     r))
+ r(.getParameter rq"c")
+ c(javax.servlet.http.Cookie."a"r)]
+ (.setMaxAge c(* 60 60 24 30))
+ (.addCookie rs c)r))
