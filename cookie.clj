80e84ab5407db388060f8d605d4db8b1d5087c25
RaWa <wasserfuhr@yahoo.com>
2017-04-02 20:35:40 +0200

    ~cookie.clj

--- cookie.clj
+++ cookie.clj
@@ -2,4 +2,6 @@
  r(.getParameter rq"c")
  c(javax.servlet.http.Cookie."0"r)]
  (.setMaxAge c(* 60 60 24 30))
- (.addCookie rs c)r))
+ (.addCookie rs c)
+ (apply str(map(fn[c](str(subs(.getName c)0 8)" "))
+  (.getCookies rq)))))
