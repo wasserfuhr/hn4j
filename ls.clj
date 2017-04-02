43583471d9b126b01b6d94357ab50a725443b891
RaWa <wasserfuhr@yahoo.com>
2017-04-03 00:15:38 +0200

    ~ls.clj

--- ls.clj
+++ ls.clj
@@ -1,4 +1,4 @@
 (fn[rq rs]
-(apply str(map(fn[f](let[l(.getName f)](format"\"%x %05x %s\n"
+(str"[\n"(apply str(map(fn[f](let[l(.getName f)](format"\"%x %05x %s\",\n"
 (-(/(.lastModified f)1000)0x5608aa2b)
-(.length f)l)))(.listFiles(java.io.File."a")))))
+(.length f)l)))(.listFiles(java.io.File."a"))))"]")
