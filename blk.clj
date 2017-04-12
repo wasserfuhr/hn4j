41594751254d1a28f1e5a42a747d0b8eb01b1745
RaWa <wasserfuhr@yahoo.com>
2017-04-13 00:11:47 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -3,5 +3,5 @@
  h(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
 ]h
 
-(clojure.data.json/read-str "{'a':2}" :key-fn keyword)
+(clojure.data.json/read-str "{\"a\":2}" :key-fn keyword)
 ))
