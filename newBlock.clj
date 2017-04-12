4fa31e01022654c27f780800b831d0a57223753e
RaWa <wasserfuhr@yahoo.com>
2017-04-12 23:49:36 +0200

    ~newBlock.clj

--- newBlock.clj
+++ newBlock.clj
@@ -1,6 +1,9 @@
 (fn[rq rs](let[
  c(subs(.getRequestURI rq)5)
  h"";(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
-]
-(spit"haha"(.getParameter rq"height"):append true)
+ ]
+(spit"haha"
+;(.getParameter rq"height")
+(slurp(.getReader rq))
+:append true)
 h))
