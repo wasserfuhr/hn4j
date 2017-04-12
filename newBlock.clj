25282c5f7b6f62fe8ecdb697488b6e1c4b0d6338
RaWa <wasserfuhr@yahoo.com>
2017-04-13 00:28:20 +0200

    ~newBlock.clj

--- newBlock.clj
+++ newBlock.clj
@@ -1,9 +1,7 @@
 (fn[rq rs](let[
- c(subs(.getRequestURI rq)5)
+ c"";(subs(.getRequestURI rq)5)
  h"";(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
+ b(clojure.data.json/read-str(slurp(.getReader rq)):key-fn keyword)
+ h(:hash b);(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
  ]
-(spit"haha"
-;(.getParameter rq"height")
-(slurp(.getReader rq))
-:append true)
-h))
+(spit(str"blk/"(:height b))h)))
