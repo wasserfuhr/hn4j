f50ef3b34fbfa9f9938168efb4637fe20cc12449
RaWa <wasserfuhr@yahoo.com>
2017-04-14 00:08:01 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -1,9 +1,14 @@
 (fn[rq rs](let[
  c"";(subs(.getRequestURI rq)5)
- h"";(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
+ h;"";(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
+  (slurp"https://blockchain.info/block/00000000000000000068985802eae0a68a0f3739a94df93cf1496d67ac84ffd7?format=json")
  lh(slurp"bcHead")
 ;  (spit(str"blk/"hb)bh)
   ;(if(<
 ]
-(> 0(.compareTo lh"70b4e"))))
+(> 0(.compareTo lh"70b4e"))
+
+(count(:tx(clojure.data.json/read-str h :key-fn keyword)))
+
+))
 ;(format"%x"(:a (clojure.data.json/read-str "{\"a\":16}" :key-fn keyword)))))
