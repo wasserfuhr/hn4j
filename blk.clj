a9cf164d4a9a55887024ad6c83210a8cceee2c63
RaWa <wasserfuhr@yahoo.com>
2017-04-14 16:01:18 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -2,10 +2,11 @@
  c(filter(fn[c](="0"(.getName c)))(.getCookies rq))
  h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
    (. h update m)(.digest h)))
- f(fn[h](apply str(map #(format"%02x"(bit-and % 0xff))h)))]
- (str"Hi "
- (if(="693c599b0e27c7ad6dd3564ec14d5336b9b9c2b2183c0ec32ddb0d6565d55ca9"
-  (f(h(.getBytes(.getValue(first c))))))"RaWa" "Visitor"))))
+ f(fn[h](apply str(map #(format"%02x"(bit-and % 0xff))h)))
+ u(if(>(count c)0)
+   (if(="693c599b0e27c7ad6dd3564ec14d5336b9b9c2b2183c0ec32ddb0d6565d55ca9"
+    (f(h(.getBytes(.getValue(first c)))))))"RaWa")]
+ (str"Hi "(if u u"Visitor"))))
 ; (.getName(first c))))
 ; lh(slurp"bcHead")
 ;(spit(str"blk/"hb)bh)]
