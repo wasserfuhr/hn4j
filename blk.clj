f0ac3476b73bffd3ec4c8f7b535e731def868c14
RaWa <wasserfuhr@yahoo.com>
2017-04-14 17:34:42 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -1,8 +1,8 @@
 (fn[rq rs](let[
- c(filter(fn[c](="0"(.getName c)))(.getCookies rq))
  h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
    (. h update m)(.digest h)))
  f(fn[h](apply str(map #(format"%02x"(bit-and % 0xff))h)))
+ c(filter(fn[c](="0"(.getName c)))(.getCookies rq))
  u(if(>(count c)0)
    (if(="693c599b0e27c7ad6dd3564ec14d5336b9b9c2b2183c0ec32ddb0d6565d55ca9"
     (f(h(.getBytes(.getValue(first c))))))"RaWa"))]
