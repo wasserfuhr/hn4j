41a2ea5d846b76c75b97cc881d795f4538949bb0
RaWa <wasserfuhr@yahoo.com>
2017-04-14 15:16:17 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -3,6 +3,7 @@
  h;"";(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
   (slurp"https://blockchain.info/block/00000000000000000068985802eae0a68a0f3739a94df93cf1496d67ac84ffd7?format=json")
  lh(slurp"bcHead")
+  (apply str(map(fn[c](str"\n"(.getName c)"="(subs(str(.getValue c)"       ")0 8)" "))
 ;  (spit(str"blk/"hb)bh)
   ;(if(<
 ]
