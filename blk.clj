cd3ea9272ed1459c252be339c9972318c597fe8d
RaWa <wasserfuhr@yahoo.com>
2017-04-14 15:47:06 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -3,7 +3,9 @@
  h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
    (. h update m)(.digest h)))
  f(fn[h](apply str(map #(format"%02x"(bit-and % 0xff))h)))]
-(f(h(.getBytes(.getValue(first c)))))))
+ (str"Hi "
+ (if(="693c599b0e27c7ad6dd3564ec14d5336b9b9c2b2183c0ec32ddb0d6565d55ca9"
+  (f(h(.getBytes(.getValue(first c))))))"RaWa" "Visitor"))))
 ; (.getName(first c))))
 ; lh(slurp"bcHead")
 ;(spit(str"blk/"hb)bh)]
