ac69dce387fb33d28a631e3c7d83e298923f3c88
RaWa <wasserfuhr@yahoo.com>
2017-04-14 15:26:14 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -1,16 +1,14 @@
 (fn[rq rs](let[
  c"";(subs(.getRequestURI rq)5)
- h;"";(subs(slurp(str"https://blockchain.info/rawblock/"c"?format=hex"))0 160)
   (slurp"https://blockchain.info/block/00000000000000000068985802eae0a68a0f3739a94df93cf1496d67ac84ffd7?format=json")
  lh(slurp"bcHead")
  c(filter(fn[c](="0"(.getName c)))(.getCookies rq))
-;  (spit(str"blk/"hb)bh)
-  ;(if(<
-]
-(> 0(.compareTo lh"70b4e"))
-(apply str
-(map(fn[t](str(:hash t)"\n"))
- (:tx(clojure.data.json/read-str h :key-fn keyword)))))
-
-))
+ h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
+   (. h update m)(.digest h)))
+ f(fn[h](apply str(map#(format"%02x"(bit-and % 0xff))h)))
+      ;  (spit(str"blk/"hb)bh)]
+;(> 0(.compareTo lh"70b4e"))
+(f(h(.getBytes c)))))
+;map(fn[t](str(:hash t)"\n"))
+ ;(:tx(clojure.data.json/read-str h :key-fn keyword)))))
 ;(format"%x"(:a (clojure.data.json/read-str "{\"a\":16}" :key-fn keyword)))))
