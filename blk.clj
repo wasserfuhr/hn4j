c60c386f4814752c0ff4246a93ab3f4cc23b9c53
RaWa <wasserfuhr@yahoo.com>
2017-04-14 15:32:16 +0200

    ~blk.clj

--- blk.clj
+++ blk.clj
@@ -4,11 +4,11 @@
  h(fn[m](let[h(java.security.MessageDigest/getInstance"SHA-256")]
    (. h update m)(.digest h)))
  f(fn[h](apply str(map#(format"%02x"(bit-and % 0xff))h)))]
-      ;  (spit(str"blk/"hb)bh)]
-(f(h(.getBytes(first c)))))
+(f(h(.getBytes(first c))))))
+;(spit(str"blk/"hb)bh)]
 ;(> 0(.compareTo lh"70b4e"))
-; c"";(subs(.getRequestURI rq)5)
-;  (slurp"https://blockchain.info/block/00000000000000000068985802eae0a68a0f3739a94df93cf1496d67ac84ffd7?format=json")
+;c(subs(.getRequestURI rq)5)
+; (slurp"https://blockchain.info/block/00000000000000000068985802eae0a68a0f3739a94df93cf1496d67ac84ffd7?format=json")
 ;map(fn[t](str(:hash t)"\n"))
  ;(:tx(clojure.data.json/read-str h :key-fn keyword)))))
 ;(format"%x"(:a (clojure.data.json/read-str "{\"a\":16}" :key-fn keyword)))))
