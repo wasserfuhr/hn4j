23fd340bba90b20a149bc02b72ed29d0b4aa7559
RaWa <wasserfuhr@yahoo.com>
2017-04-15 15:43:37 +0200

    ~test.clj

--- test.clj
+++ test.clj
@@ -5,4 +5,10 @@
  ;(spit(str"ad"(subs(format"%x" d)0 3)".log")
 ;  (format"%x.%02x %s\n"d(quot(* 256 r)1000) "test")
   ;:append true)
+  ;(> 0(.compareTo lh"70b4e"))
+  ;c(subs(.getRequestURI rq)5)
+  
+ (map(fn[t](str(:hash t)"\n"))
+  (:tx(clojure.data.json/read-str(slurp"bb/70c25"):key-fn keyword)))
+  
   ))
