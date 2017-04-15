0bea90bd8c2994dcd367aae70600048ef1a80017
RaWa <wasserfuhr@yahoo.com>
2017-04-16 00:42:03 +0200

    wget https://raw.githubusercontent.com/wasserfuhr/AllHashes/master/Ur.java

---
+++ Ur.java
@@ -0,0 +1,15 @@
+public class Ur extends Thread{
+ public void run(){try{
+  long lastSec=0;
+  while(true){
+   long c=System.currentTimeMillis();
+   long t=(lastSec+1)*1000-c;
+   if(t<=0){
+    System.out.println(c);
+    Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
+    lastSec=c/1000;
+   }else{
+    sleep(t/2);}}
+  }catch(Exception e){System.err.println(e.getMessage());}}
+ public static void main(String[] a)throws Exception{
+  new Ur().start();}}
