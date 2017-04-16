8235ca40cd63cacd334ea72a33725294787782bf
RaWa <wasserfuhr@yahoo.com>
2017-04-16 04:14:04 +0200

    ~index.clj

--- index.clj
+++ index.clj
@@ -7,48 +7,81 @@
   #(format "%02x"(bit-and % 0xff))h)))
  ua(.getHeader rq"User-Agent")
  uh(f(h(.getBytes ua)))]
-(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"&alpha; &laquo; &beta;"]
-[:meta{:name"viewport":content"width=device-width,initial-scale=1.0"}]][:body
-;[:div{:style"overflow:hidden;height:60px;width:750px"}
-[:div#rot;{:style"overflow:hidden;height:60px;width:900px;margin-let:-50px"}
-(map(fn[i]
- [:img{:style"height:60px;width:45px"}]
- )(range 64))
-];]
-[:p"Hi "(subs uh 0 6)"@"i"!"]
+(hiccup.core/html"<!DOCTYPE html>"[:html
 
-[:p"You are now OnBoard "[:a{:href"https://github.com/wasserfuhr/hn4j/blob/master/Jet.java"}"AlphaJet"]"."]
 
-[:p"Soon we will be "[:a{:href"https://floatingboat2013.appspot.com/ef/OnAlpha"}"&beta;"]"."]
+[:head[:title#ti t" « α"][:style{:type"text/css"}"
+body{
+ background:#000;
+ color:#0f0;
+ font-family:monospace}
+a{color:#0f1}"]]
+[:body
+ [:canvas#ac{:width 256 :height 16}][:br]
+  [:div#say]
+   [:hr]
+    "α"((:pageEdit v)(str"/"t)t 8)
+     [:small
+       [:a{:href"https://floatingboat2013.appspot.com/ad"}"ad"]" "
+         [:a{:href"https://floatingboat2013.appspot.com/"}"fl"]" "
+	   [:a{:href"https://sl4.eu/hls"}"hls"]" "
+	     [:a{:href"https://sl4.eu/ls"}"ls"]" "
+	       [:a{:href"https://floatingboat2013.appspot.com/UrPic"}"ur"]]
+	        [:script"try{
+var start=new Date().getTime()
+var bt='"(subs bt 13)"'
+var c=document.getElementById('ac').getContext('2d')
+var ti=document.getElementById('ti')
+var s=document.getElementById('say')
+function bits(v,x,y){
+ for(var i=0;i<4;i++)
+  if(v&(1<<i))
+   c.fillRect(x*64+i*16,y*16,16,16)}
+function t(){try{
+ var t0=Math.floor((new Date().getTime()-start)/1000)
+ if([2,4,8,16,32,64,128,256,512].indexOf(t0)>0){
+ var l=0
+ xhr=new XMLHttpRequest()
+ try{
+  xhr.open('get','https://sl4.eu/alive')
+  xhr.onreadystatechange=function(){
+   if(xhr.readyState===4)
+    if(xhr.status===200){
+     h=''}//xhr.responseText.split('.')
+    else
+     console.log('Error: '+xhr.status)}
+  xhr.send(null)}
+ catch(e){console.log('Error: '+xhr.status)}}
+ c.fillStyle='#000'
+ c.fillRect(0,0,256,16)
+ c.fillStyle='#0f0'
+ try{
+  ti.innerHTML='BitCount « αt'+t0.toString(16)
+ }catch(e){}
+ say={2:'...',
+4:'What is your name? <input/>.',
+6:'WelCome!',
+8:'What you see here is called a BinaryCounter.',
+12:'The left most cube changes every second,',
+16:'the next one every 2 seconds,',
+20:'the next one every 4 seconds,',
+24:'the next one every 8 seconds,',
+28:'the next one every 16 seconds,',
+32:'and so on.',
+40:'On your SmartPhone or LapTop, a BinaryCounter works at least 1000 times per second.',
+42:'What is your name? <input/>.',
+256:'<a href=a>de</a>.'}
+ if(say[t0])
+  s.innerHTML=say[t0]+'<br/>'+s.innerHTML;
+ for(var i=0;i<7;i++){
+  var v=(t0>>(4*i))&15
+  bits(v,i,0)}
+// c.strokeStyle='#0f0'
+// c.strokeRect(0,0,448,48)
+// for(i=1;i<8;i++)
+//  c.strokeRect(i*32,0,0.5,2048)
+}catch(e){alert(e)}}
+setInterval(t,1000)
+}catch(e){alert(e)}"]
 
-[:br][:br]"Currently, we are all into faces. Try "
-[:a{:href"https://sl4.eu/age"}"History"]" "
-[:a{:href"https://sl4.eu/math"}"Math"]" "
-[:a{:href"https://sl4.eu/memo"}"Memory"]" "
-[:a{:href"https://sl4.eu/phim"}"Physics"]" "
-[:a{:href"https://sl4.eu/tap"}"Tapestry"]
-;[:script{:src"https://rawgit.com/wasserfuhr/hn4j/master/hilde.js"}]
-[:script(slurp"hilde.js")]
-[:script"try{
-var ct=0;
-var now=new Date().getTime()
-setInterval(function(){
-try{
- ct++
- //if(ct<2)alert(1)
- var i=document.getElementById('rot').childNodes[Math.floor(64*Math.random())]
- i.src='https://upload.wikimedia.org/wikipedia/commons/thumb/'+hpic[ct%128]
-// i.src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/William_Shatner_at_Comic-Con_2012_cropped.jpg/214px-William_Shatner_at_Comic-Con_2012_cropped.jpg'
- i.style='height:60px;width:45px'
-}catch(e){alert(e)}
-
-},1000)
-}catch(e){alert(e)}
-"]
-[:p"pro: "
-[:a{:href"https://floatingboat2013.appspot.com/ad"}"ad"]" "
-[:a{:href"https://floatingboat2013.appspot.com/"}"fl"]" "
-[:a{:href"/hls"}"hls"]" "
-[:a{:href"/ls"}"ls"]" "
-[:a{:href"https://floatingboat2013.appspot.com/UrPic"}"ur"]
-]]])))
+])))
