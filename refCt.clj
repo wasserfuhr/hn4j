fb953093cd4d9b617909c1da39617df03f79d96f
RaWa <wasserfuhr@yahoo.com>
2017-04-16 22:29:35 +0200

    ~refCt.clj

--- refCt.clj
+++ refCt.clj
@@ -5,5 +5,5 @@ q(read-string(str"0x"l))
 s(slurp(str"q/"l))]
  (str l" "(count(keys(:sitelinks
   ((keyword(str"Q"q))(:entities(json/read-str s :key-fn keyword))))))"\n")))
- (.listFiles(java.io.File."q")))))
-;  (.split(slurp"wd/0331")" ")))))
+; (.listFiles(java.io.File."q")))))
+  (.split(slurp"wd/0416")" ")))))
