88280f34f9fca7ce1cdd703b5b9e6ff889d2ef1f
RaWa <wasserfuhr@yahoo.com>
2017-04-16 22:06:21 +0200

    ~wdbd.clj

--- wdbd.clj
+++ wdbd.clj
@@ -1,8 +1,8 @@
 (ns x(:require[clojure.data.json :as json]))
-(let[m"04"d"01"
+(let[m"04"d"16"
 l(count"http://www.wikidata.org/entity/Q")
 e(java.net.URLEncoder/encode
- (str"select?e where{?e wdt:P31 wd:Q5.?e wdt:P569 ?d.filter(MONTH(?d)="m"&&DAY(?d)="d")}limit 20000"))
+ (str"select?e where{?e wdt:P31 wd:Q5;wdt:P569 ?d.filter(month(?d)="m"&&day(?d)="d")}limit 50000"))
 q(str"https://query.wikidata.org/sparql?query="e"&format=json")]
 (spit(str"wd/"m d)(.trim(apply str
  (map(fn[i](format"%x "(Long.(subs(:value(:e i))l))))
