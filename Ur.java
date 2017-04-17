44a80712f650ae41634e009437f31489d1c5e280
wasserfuhr <wasserfuhr@yahoo.com>
2017-04-17 20:28:46 +0200

    ~Ur.java

--- Ur.java
+++ Ur.java
@@ -7,11 +7,9 @@ public class Ur extends Thread{
    long t=(lastSec+1)*1000-c;
    if(t<=0){
     //http://stackoverflow.com/questions/1625234/how-to-append-text-to-an-existing-file-in-java
-       PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("myfile.txt", true)));
-       out.println("the text");
-       out.close();
-
-    System.out.println(c);
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ur"+".log", true)));
+    out.println(c);
+    out.close();
     Runtime.getRuntime().exec("/root/git/hn4j/job.sh");
     lastSec=c/1000;
    }else{
