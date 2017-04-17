b5583410cf31759be495f1224bbd13084d03ff56
RaWa <wasserfuhr@yahoo.com>
2017-04-17 16:47:03 +0200

    ~exec.clj

--- exec.clj
+++ exec.clj
@@ -7,5 +7,6 @@
   (if(="693c599b0e27c7ad6dd3564ec14d5336b9b9c2b2183c0ec32ddb0d6565d55ca9"
    (f(h(.getBytes(.getValue(first c))))))"RaWa"))]
  (if u(do
- (spit"exec.sh"(subs(.getRequestURI rq)6))
+ (spit"exec.sh"
+  (java.net.URLDecoder/decode(subs(.getRequestURI rq)6)))
  (slurp(.getInputStream(.exec(Runtime/getRuntime)"/root/git/hn4j/exec.sh")))))))
