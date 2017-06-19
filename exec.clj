e0933b9dc51587c20cfafa991836217afcf7d19d
wasserfuhr <wasserfuhr@yahoo.com>
2017-06-19 15:42:33 +0200

    ~exec.clj

--- exec.clj
+++ exec.clj
@@ -5,7 +5,7 @@
  c0(filter(fn[c](="1"(.getName c)))(.getCookies rq))
  rc(.getParameter rq"c")
  c(if rc [rc]c0)
- v(if rc rc(.getValue(first c)))
+ v(if rc rc(.getValue(first c0)))
  u(if(>(count c)0)
   (if(="693c599b0e27c7ad6dd3564ec14d5336b9b9c2b2183c0ec32ddb0d6565d55ca9"
    (f(h(.getBytes v)))"RaWa"))]
